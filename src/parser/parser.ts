e2ee82e68d7c84b9ecc1feb76e2c040c342b2557
Jack Wilburn <jackwilburn@tutanota.com>
2022-11-21 06:55:15 -0700

    Add preliminary parsing logic

---
+++ src/parser/parser.ts
@@ -0,0 +1,23 @@
+import { parse as hjsonParse } from 'hjson';
+import { StudyConfig } from './types';
+
+export function parseStudyConfig(fileData: string) {
+    let data = hjsonParse(fileData);
+    
+    if (validateStudyConfig(data)) {
+        return data;
+    } else {
+        throw Error("There was an issue validating your file");
+    }
+}
+
+function validateStudyConfig(obj: unknown): obj is StudyConfig {
+    // TODO: actually check if we have a valid config
+    // throw Error("There was an issue validating your file");
+    return true;
+}
+
+// We can use this type of logic to check the subpieces of the template
+// function isA(obj: unknown): obj is A {
+//     // return boolean here
+// }
---
+++ src/parser/types.ts
@@ -0,0 +1,81 @@
+interface StudyMetadata {
+    title: string;
+    version: string;
+    author: string[];
+    date: Date;
+    description?: string;
+    organization?: string[];
+}
+
+interface StudyComponents {
+    [key: string]: StudyComponent;
+}
+
+interface StudyComponent {
+    type: 'consent' | 'training' | 'practice' | 'attention-test' | 'trials';
+}
+
+interface ConsentComponent extends StudyComponent {
+    path: string;
+}
+
+interface TrainingComponent extends StudyComponent {
+    // TODO
+}
+
+interface PracticeComponent extends StudyComponent {
+    // TODO
+}
+
+interface AttentionComponent extends StudyComponent {
+    // TODO
+}
+
+interface TrialsComponent extends StudyComponent {
+    order: string[];
+    trials: { [key: string]: Trial };
+}
+
+interface Trial {
+    description: string;
+    instruction: string;
+    stimulus: Stimulus;
+    responses: Response[];
+    answers?: Answer[];
+}
+
+interface Stimulus {
+    type: 'react-component' | 'image' | 'javascript' | 'website';
+    path?: string;
+    parameters?: { [key: string]: any };
+}
+
+// Add types for stimulus
+
+interface Response {
+    id: string;
+    prompt: string;
+    type: 'numerical' | 'short-text' | 'long-text' | 'likert';
+    placeholder: string;
+    required: boolean;
+}
+
+// Add types for response
+
+interface Answer {
+    id: string;
+    answer: any;
+    'acceptable-low'?: number;
+    'acceptable-high'?: number;
+    'answer-callback'?: string;
+    'answer-regex'?: string;
+}
+
+// Add types for answers
+
+export interface StudyConfig {
+    'config-version': number;
+    'study-metadata': StudyMetadata;
+    components: StudyComponents;
+    sequence: string[];
+}
