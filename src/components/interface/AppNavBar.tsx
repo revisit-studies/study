9d202c6265c811c547086a9fd6b1d078789c8bd9
Jack Wilburn <jackwilburn@tutanota.com>
2023-04-15 15:35:29 -0600

    Add example nav bar prompt and answers

--- src/components/interface/AppNavBar.tsx
+++ src/components/interface/AppNavBar.tsx
@@ -1,9 +1,37 @@
-import { Navbar, Text } from '@mantine/core';
+import { Button, Flex, Navbar, NumberInput, Space, Text, Textarea } from '@mantine/core';
 
 export default function AppNavBar() {
-  return (
-    <Navbar p="md" width={{ lg: 300 }} style={{ zIndex: 0 }}>
-      <Text>Application navbar</Text>
+  const trialHasSideBar = true;
+  const trialHasSideBarResponses = true;
+  const trialHasSideBarUI = false;
+
+  return trialHasSideBar ? (
+    <Navbar width={{ lg: 300 }} style={{ zIndex: 0 }}>
+      <Navbar.Section bg="gray.3" p="xl">
+        <Text c="gray.9"><Text span c="orange.8" fw={700} inherit>Task 8:</Text> Task Instructions</Text>
+      </Navbar.Section>
+
+      {trialHasSideBarResponses && 
+      <Navbar.Section bg="gray.1" p="xl" grow>
+        <NumberInput></NumberInput>
+
+        <Space h="xl"></Space>
+
+        <Textarea
+          label="Please explain your answer below"
+          placeholder="Type something..."
+          minRows={5}
+        ></Textarea>
+
+        <Space h="xl"></Space>
+
+        <Flex justify="end"><Button>Submit</Button></Flex>
+      </Navbar.Section>}
+
+      {trialHasSideBarUI && 
+      <Navbar.Section p="xl">
+        <div id="sidebar-ui"></div>
+      </Navbar.Section>}
     </Navbar>
-  );
+  ) : null;
 }
