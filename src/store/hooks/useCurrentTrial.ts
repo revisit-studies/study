4a0c378f98a93ea04f23c75a7b92072e852cecd8
Jack Wilburn <jackwilburn@tutanota.com>
2023-04-23 14:49:40 -0600

    Simplify use current trial

--- src/store/hooks/useCurrentTrial.ts
+++ src/store/hooks/useCurrentTrial.ts
@@ -1,5 +1,4 @@
 import { useLocation } from 'react-router-dom';
-import { useAppSelector } from '..';
 import { useCurrentStep } from '../../routes';
 
 /**
@@ -10,16 +9,7 @@ import { useCurrentStep } from '../../routes';
 export function useCurrentTrial() {
   const currentStep = useCurrentStep();
 
-  const { config } = useAppSelector((state) => state.study);
-
-  if (
-    currentStep.length === 0 ||
-    !config ||
-    config.components[currentStep]?.type !== 'trials'
-  )
-    return null;
-
-  const trialId = useLocation().pathname.split('/')[2]; // Assumes /<trialname>/:id
+  const trialId = useLocation().pathname.split('/')[2] || null; // Assumes /<trialname>/:id
 
   return {
     trailName: currentStep,
