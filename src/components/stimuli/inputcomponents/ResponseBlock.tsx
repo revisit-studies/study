9f2f7f6a2676f3cb83a0ede2173dc9c13309266b
Alex Dean <dingx315@hotmail.com>
2023-04-24 12:18:10 -0700

    rewrite resonse block

--- src/components/stimuli/inputcomponents/ResponseBlock.tsx
+++ src/components/stimuli/inputcomponents/ResponseBlock.tsx
@@ -1,26 +1,25 @@
 import { Response } from '../../../parser/types';
-import {saveSurvey, saveTrialAnswer, useAppDispatch} from '../../../store';
+import {saveTrialAnswer, useAppDispatch} from '../../../store';
 import ResponseSwitcher from './ResponseSwitcher';
 import {NextButton} from '../../NextButton';
 import {Group, Text, Button} from '@mantine/core';
 import {useCurrentStep} from '../../../routes';
 import {useParams} from 'react-router-dom';
 import {useNextTrialId} from '../../../controllers/utils';
-import {useForm} from '@mantine/form';
 import {useEffect, useState, useRef} from 'react';
 import {useNextStep} from '../../../store/hooks/useNextStep';
 import {useTrialStatus} from '../../../store/hooks/useTrialStatus';
 import {createAnswerField} from './utils';
-import {useSurvey} from '../../../store/hooks/useSurvey';
 
 type Props = {
     responses: Response[];
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     correctAnswer?: any;
-    type: 'trials' | 'practice' ;
+    type: 'trials' | 'practice';
 };
 
 export default function ResponseBlock({ responses, correctAnswer, type }: Props) {
+
     const dispatch = useAppDispatch();
     const currentStep = useCurrentStep();
     const nextStep = useNextStep();
@@ -30,35 +29,10 @@ export default function ResponseBlock({ responses, correctAnswer, type }: Props)
     const [disableNext, setDisableNext] = useState(true);
     if (!responses || !trialStatus || !trialId) return <></>;
 
-
-    const generateInitFields = () => {
-        let initObj = {};
-
-        responses.forEach((response) => {
-            initObj = {...initObj, [response.id]: ''};
-        });
-
-        return initObj;
-    };
-
-    const generateValidation = () => {
-        let validateObj = {};
-
-        responses.forEach((response) => {
-            if(response.required)
-                validateObj = {...validateObj, [response.id]: (value: string | undefined) => (value === undefined ? 'Empty input' : null)};
-        });
-
-        return validateObj;
-    };
-
-    const answerField = useForm({
-        initialValues: generateInitFields(),
-        validate: generateValidation(),
-    });
+    const answerField = createAnswerField(responses);
 
     const handleResponseCheck = () => {
-          setDisableNext(!disableNext);
+        setDisableNext(!disableNext);
     };
 
     useEffect(() => {
@@ -66,52 +40,27 @@ export default function ResponseBlock({ responses, correctAnswer, type }: Props)
             const ans = trialStatus.answer ? JSON.parse(trialStatus.answer as string) : {};
             answerField.setFieldValue(response.id, ans[response.id] || '');
         });
-        // {
-        //     stage === 'trial' && trialStatus && useEffect(() => {
-        //         responses.forEach((response) => {
-        //             const ans = (trialStatus.answer && typeof trialStatus.answer === 'string') ? JSON.parse(trialStatus.answer) : {};
-        //             answerField.setFieldValue(response.id, ans[response.id] || '');
-        //         });
-        //     }, [trialStatus.answer]);
-        // }
-        //
-        // {
-        //     stage === 'survey' && useEffect(() => {
-        //         for (const [key, value] of Object.entries(survey)) {
-        //             answerField.setFieldValue(key,value);
-        //         }
-        //     }, [survey]);
-        // }
     }, [trialStatus.answer]);
 
     return (
         <>
             <form onSubmit={answerField.onSubmit(console.log)}>
-            {
-                responses.map((response, index) => {
-                    return (
-                            type === 'trials'  ? <ResponseSwitcher key={index}  answer={answerField.getInputProps(response.id)} response={response} />
-                        :
-                           <ResponseSwitcher key={index} answer={answerField.getInputProps(response.id)} response={response} />
-                    );
-                })
-            }
-
-            {!disableNext && <Text>The correct answer is: {correctAnswer}</Text>}
-            <Group position="right" spacing="xs" mt="xl">
-                {!(correctAnswer === null) ? <Button onClick={handleResponseCheck} disabled={!answerField.isValid()}>Check Answer</Button> : null}
-                {nextTrailId ? (
+                {
+                    responses.map((response, index) => {
+                        return (
+                            <ResponseSwitcher key={index} status={trialStatus} answer={answerField.getInputProps(response.id)} response={response} />
+                        );
+                    })
+                }
+                {!disableNext && <Text>The correct answer is: {correctAnswer}</Text>}
+                <Group position="right" spacing="xs" mt="xl">
+                    {!(correctAnswer === null) ? <Button onClick={handleResponseCheck} disabled={!answerField.isValid()}>Check Answer</Button> : null}
                     <NextButton
                         disabled={correctAnswer !== null ? disableNext : !answerField.isValid()}
-                        to={`/${currentStep}/${nextTrailId}`}
+                        to={ nextTrailId? `/${currentStep}/${nextTrailId}` : `/${nextStep}`}
                         process={() => {
-                            if (trialStatus.complete) {
-                                answerField.setFieldValue('input', '');
-                            }
-
                             const answer = JSON.stringify(answerField.values);
                             console.log(answer,'answer');
-
                             dispatch(
                                 saveTrialAnswer({
                                     trialName: currentStep,
@@ -121,31 +70,9 @@ export default function ResponseBlock({ responses, correctAnswer, type }: Props)
                                 })
                             );
                             setDisableNext(!disableNext);
-                            answerField.setFieldValue('input', '');
                         }}
                     />
-                ) : (
-                    <NextButton
-                        to={`/${nextStep}`}
-                        disabled={correctAnswer === null || disableNext}
-                        process={() => {
-                            // complete trials
-                        }}
-                    />
-                )}
-
-                    {/*{type === 'survey' && <NextButton*/}
-                    {/*    // disabled={!answerField.isValid()}*/}
-                    {/*    to={`/${nextStep}`}*/}
-                    {/*    // disabled={correctAnswer === null || disableNext}*/}
-                    {/*    process={() => {*/}
-                    {/*        dispatch(*/}
-                    {/*            saveSurvey(answerField.values)*/}
-                    {/*        );*/}
-                    {/*    }}*/}
-                    {/*/>}*/}
-
-            </Group>
+                </Group>
             </form>
         </>
     );
