519decaac4550d204de88330d46907b653f44729
Jack Wilburn <jackwilburn@tutanota.com>
2023-04-25 14:19:31 -0600

    Fix issues with base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? 'revisit-studies/' : '/',
+  base: command === 'build' ? '/revisit-studies-frontend/' : '/',
   plugins: [
     react(),
   ],
