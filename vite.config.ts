72ee3658be0e72b538a0f1e6e72a553e6fb3e0df
Jack Wilburn <jackwilburn@tutanota.com>
2023-04-25 14:22:20 -0600

    Fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/revisit-studies-frontend/' : '/',
+  base: command === 'build' ? '/revisit-study-frontend/' : '/',
   plugins: [
     react(),
   ],
