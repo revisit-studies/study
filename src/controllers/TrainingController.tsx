2644a501a3a040274236a10c6922d15213f4a0d6
Alex Dean <dingx315@hotmail.com>
2023-04-30 19:05:49 -0400

    remove unused input

--- src/controllers/TrainingController.tsx
+++ src/controllers/TrainingController.tsx
@@ -1,25 +1,18 @@
-import { Suspense, useEffect } from 'react';
-import ReactMarkdown from 'react-markdown';
-
-import { Trial} from '../parser/types';
-import { useCurrentStep } from '../routes';
+import { Suspense } from 'react';
 
 import {
   createTrialProvenance,
   TrialProvenanceContext,
 } from '../store/trialProvenance';
 import IframeController from './IframeController';
-import ReactComponentController from './ReactComponentController';
-import ResponseBlock from '../components/response/ResponseBlock';
+
 import ImageController from './ImageController';
-import { resetResponseBlockValidation, useFlagsDispatch } from '../store/flags';
 import {useTrainingConfig, useTrialsConfig} from './utils';
 
 // current active stimuli presented to the user
 
 export default function TrainingController() {
   const trialProvenance = createTrialProvenance();
-  const flagStoreDispatch = useFlagsDispatch();
   const trainingConfig = useTrainingConfig();
   const trainingStimulus = trainingConfig?.stimulus;
   console.log(trainingConfig,'trainingConfig');
