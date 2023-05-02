d5db992597c28c3c337613f30e00c05490a7bd4c
Jack Wilburn <jackwilburn@tutanota.com>
2023-05-02 10:32:16 -0600

    Fix issue with navigation in admin panel

--- src/components/interface/StepsPanel.tsx
+++ src/components/interface/StepsPanel.tsx
@@ -2,8 +2,12 @@ import { Badge, Group } from '@mantine/core';
 import { useCurrentStep } from '../../routes';
 import { useAppSelector } from '../../store';
 import { useNavigateWithParams } from '../../utils/useNavigateWithParams';
+import { useParams } from 'react-router-dom';
 
 export function StepsPanel() {
+  const { studyId = null } = useParams<{
+    studyId: string;
+  }>();
   const currentStep = useCurrentStep();
   const navigate = useNavigateWithParams();
 
@@ -20,7 +24,7 @@ export function StepsPanel() {
             cursor: 'pointer',
           }}
           color={step === currentStep ? 'red' : 'blue'}
-          onClick={() => navigate(`/${step}`)}
+          onClick={() => navigate(`/${studyId}/${step}`)}
           size="xs"
         >
           {step}
