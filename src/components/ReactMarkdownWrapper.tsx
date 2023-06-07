caeb74489cf57ee3d231bca5aee3902e2eebff72
Jack Wilburn <jackwilburn@tutanota.com>
2023-06-07 14:23:41 -0600

    Add some bottom padding to make paragraphs more apparent

--- src/components/ReactMarkdownWrapper.tsx
+++ src/components/ReactMarkdownWrapper.tsx
@@ -5,16 +5,16 @@ import { ReactNode } from 'react';
 export default function ReactMarkdownWrapper({ text }: { text: string; }) {
   const components = {
     img: ({ node, ...props }: { node: unknown; }) => <Image {...props} />,
-    p: ({ node, ...props }: { node: unknown; }) => <Text {...props} />,
-    h1: ({ node, ...props }: { node: unknown; }) => <Title order={1} {...props} />,
-    h2: ({ node, ...props }: { node: unknown; }) => <Title order={2} {...props} />,
-    h3: ({ node, ...props }: { node: unknown; }) => <Title order={3} {...props} />,
-    h4: ({ node, ...props }: { node: unknown; }) => <Title order={4} {...props} />,
-    h5: ({ node, ...props }: { node: unknown; }) => <Title order={5} {...props} />,
-    h6: ({ node, ...props }: { node: unknown; }) => <Title order={6} {...props} />,
+    p: ({ node, ...props }: { node: unknown; }) => <Text {...props} pb={8}/>,
+    h1: ({ node, ...props }: { node: unknown; }) => <Title order={1} {...props} pb={12} />,
+    h2: ({ node, ...props }: { node: unknown; }) => <Title order={2} {...props} pb={12} />,
+    h3: ({ node, ...props }: { node: unknown; }) => <Title order={3} {...props} pb={12} />,
+    h4: ({ node, ...props }: { node: unknown; }) => <Title order={4} {...props} pb={12} />,
+    h5: ({ node, ...props }: { node: unknown; }) => <Title order={5} {...props} pb={12} />,
+    h6: ({ node, ...props }: { node: unknown; }) => <Title order={6} {...props} pb={12} />,
     a: ({ node, ...props }: { node: unknown; }) => <Anchor {...props} />,
-    ul: ({ node, ...props }: { node: unknown; children: ReactNode; }) => <List withPadding {...props} />,
-    ol: ({ node, ...props }: { node: unknown; children: ReactNode; }) => <List type="ordered" withPadding {...props} />,
+    ul: ({ node, ...props }: { node: unknown; children: ReactNode; }) => <List withPadding {...props} pb={8} />,
+    ol: ({ node, ...props }: { node: unknown; children: ReactNode; }) => <List type="ordered" withPadding {...props} pb={8} />,
   };
 
   return <>
