0a454ef0a8353b43d60418bde9dbdd8157eaa8c6
Alex Dean <dingx315@hotmail.com>
2023-11-04 11:07:39 -0600

    update study end and link to prolific for mini vlat

--- src/components/StudyEnd.tsx
+++ src/components/StudyEnd.tsx
@@ -1,34 +1,23 @@
 import { Center, Flex, Text } from '@mantine/core';
 import { useStudyConfig } from '../store/hooks/useStudyConfig';
+import ReactMarkdownWrapper from './ReactMarkdownWrapper';
 
 export function StudyEnd() {
   const config = useStudyConfig();
 
-  const generateMessage = (msg : string) => {
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
-
-    // Replace all matched links with an empty string
-    const textWithoutLinks = msg.replace(urlRegex, '');
-    const link =  msg.match(urlRegex) || [];
-    return <>
-      <p>{textWithoutLinks} </p>
-      {link.length>0 && <a href={link[0]}>{link}</a>}
-    </>;
-  };
-
   return (
-    <>
-      <Center style={{ height: '100%' }}>
-        <Flex direction="column">
-          <Center>
-            <Text size="xl" display="block">
-              {config.uiConfig.studyEndMsg
-                ? generateMessage(config.uiConfig.studyEndMsg)
-                : 'Thank you for completing the study. You may close this window now.'}
-            </Text>
-          </Center>
-        </Flex>
-      </Center>
-    </>
+      <>
+        <Center style={{ height: '100%' }}>
+          <Flex direction="column">
+            <Center>
+              <Text size="xl" display="block">
+                {config.uiConfig.studyEndMsg
+                    ? <ReactMarkdownWrapper text={config.uiConfig.studyEndMsg} />
+                    : 'Thank you for completing the study. You may close this window now.'}
+              </Text>
+            </Center>
+          </Flex>
+        </Center>
+      </>
   );
 }
