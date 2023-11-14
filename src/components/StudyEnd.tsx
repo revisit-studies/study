1b7f94b11deb281a3fc8734ab46ea0a3817f4d3a
Jack Wilburn <jackwilburn@tutanota.com>
2023-11-14 15:01:56 -0700

    Fix render blocking issue with study end

--- src/components/StudyEnd.tsx
+++ src/components/StudyEnd.tsx
@@ -1,23 +1,10 @@
 import { Center, Flex, Text } from '@mantine/core';
 import { useStudyConfig } from '../store/hooks/useStudyConfig';
-import {useEffect} from 'react';
-import {useFirebase} from '../storage/init';
-import {useCreatedStore} from '../store/store';
 import ReactMarkdownWrapper from './ReactMarkdownWrapper';
 
 
 export function StudyEnd() {
   const config = useStudyConfig();
-  const firebase = useFirebase();
-  const { trrack } = useCreatedStore();
-
-  useEffect(() => {
-    async function fn() {
-      await firebase.completeSession(trrack.root.id);
-    }
-
-    fn();
-  }, []);
 
   return (
       <>
