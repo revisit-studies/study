6a357dc256c3045cf102d6ce397246e840c6b562
Jack Wilburn <jackwilburn@tutanota.com>
2023-11-14 14:29:22 -0700

    Fix up all the issue with tests

--- tests/vlat.spec.ts
+++ tests/vlat.spec.ts
@@ -2,12 +2,15 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('/');
-  await page.getByRole('button', { name: 'VLAT (Visualization Literacy Assessment Test) A replication of the VLAT questionnaire, using image stimuli.' }).click();
+  await page.getByRole('button', { name: 'Randomized Full VLAT (Visualization Literacy Assessment Test)' }).click();
   
   await page.getByRole('heading', { name: 'What is VLAT?' }).click();
   await page.getByRole('button', { name: 'Next' }).click();
 
-  const questionArray = new Array(52).fill(null);
+  await page.getByPlaceholder('Your Initials').fill('test');
+  await page.getByRole('button', { name: 'Next' }).click();
+
+  const questionArray = new Array(53).fill(null);
   for (const idx of questionArray) {
     const questionText = await page.getByText('Task:');
     await expect(questionText).toBeVisible();
@@ -17,6 +20,11 @@ test('test', async ({ page }) => {
     await page.getByRole('button', { name: 'Next' }).click();
   }
 
-  // Check that the thank you message is displayed
-  await page.getByText('Thank you for completing the study. You may close this window now.').click();
+  await page.getByPlaceholder('Enter your answer here.').click();
+  await page.getByPlaceholder('Enter your answer here.').fill('no');
+  await page.getByLabel('Did anything not render or display properly? *').fill('no');
+  await page.getByLabel('Any other issues or anything you would like to tell us? *').fill('no');
+  await page.getByRole('button', { name: 'Next' }).click();
+
+  await page.getByText('Thank you for completing the study. You may click this link and return to Prolif').click();
 });
