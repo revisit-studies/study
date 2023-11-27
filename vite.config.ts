0e08b0f2b8eaedb501d12e81de08e932692d693d
Jack Wilburn <jackwilburn@tutanota.com>
2023-11-27 10:54:33 -0700

    Bump some dependencies to newest versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -9,7 +9,7 @@ export default defineConfig(({ command, mode }) => {
   return {
     base: command === 'build' ? env.VITE_BASE_PATH : '/',
     plugins: [
-      react(),
+      react({ devTarget: 'es2022' }),
     ],
   };
 });
