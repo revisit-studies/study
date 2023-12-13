c603a4b075700d8dfa2e259a14bcb0302a24a282
Jack Wilburn <jackwilburn@tutanota.com>
2023-12-13 15:24:18 -0700

    Only have the back button disabled in prod if not in admin=t

--- src/utils/disableBrowserBack.tsx
+++ src/utils/disableBrowserBack.tsx
@@ -11,10 +11,14 @@ export function disableBrowserBack() {
   const storeDispatch = useStoreDispatch();
 
   useEffect(() => {
-    window.history.pushState(null, '', window.location.href);
-    window.onpopstate = () => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const isAdmin = (searchParams.get('admin') || 'f') === 't';
+
+    if (import.meta.env.DEV && !isAdmin)
       window.history.pushState(null, '', window.location.href);
-      storeDispatch(setAlertModal({ show: true, message: 'Using the browser\'s back button is prohibited during the study.' }));
-    };
-  }, [currentStep, setAlertModal, storeDispatch]);
+      window.onpopstate = () => {
+        window.history.pushState(null, '', window.location.href);
+        storeDispatch(setAlertModal({ show: true, message: 'Using the browser\'s back button is prohibited during the study.' }));
+      };
+    }, [currentStep, setAlertModal, storeDispatch]);
 }
