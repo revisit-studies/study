462072c6a4faf6f16ebb21b8a0d67a1de6148277
Jack Wilburn <jackwilburn@tutanota.com>
2024-04-02 15:45:49 -0600

    Use useParams, instead of manually getting the pathname

--- src/analysis/AnalysisInterface.tsx
+++ src/analysis/AnalysisInterface.tsx
@@ -1,14 +1,13 @@
 import { AppShell, Container } from '@mantine/core';
 
-import { useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import AppHeader from './components/interface/AppHeader';
 import AppNav from './components/interface/AppNav';
 import { GlobalConfig } from '../parser/types';
 import { SummaryBlock } from './dashboard/SummaryBlock';
 
 export function AnalysisInterface(props: { globalConfig: GlobalConfig; }) {
-  const location = useLocation();
-  const page = location.pathname.split('/')[2];
+  const { page } = useParams();
 
   return (
     <AppShell>
