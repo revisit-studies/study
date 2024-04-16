15fae447b6490db382ae667afa55aa978fa53ae1
Alex Dean <dingx315@hotmail.com>
2024-04-16 00:20:21 -0700

    cp- categorical answer vis

--- src/analysis/stats/components/AnswerPanel.tsx
+++ src/analysis/stats/components/AnswerPanel.tsx
@@ -1,15 +1,16 @@
 import {
-  Box, Container, Group, Title,
+  Box, Container, Group, Stack, Title,
 } from '@mantine/core';
 import React, { useEffect, useState } from 'react';
 import { VegaLite } from 'react-vega';
-import { IndividualComponent, InheritedComponent } from '../../../parser/types';
+import { IndividualComponent, InheritedComponent, RadioResponse } from '../../../parser/types';
 
 export default function AnswerPanel(props: { data: Record<string, Record<string, unknown>>, trialName: string, config: IndividualComponent | InheritedComponent | undefined}) {
   const { data, config, trialName } = props;
   const [correctUser, setCorrectUser] = useState<string[]>([]);
   const [incorrectUser, setIncorrectUser] = useState<string[]>([]);
-
+  const [categoricalStats, setCategoricalStats] = useState<{option:string, count:number}[]>([]);
+  const [correctValue, setCorrectValue] = useState<string>('');
   useEffect(() => {
     const responses = config?.response;
     const correct:string[] = [];
@@ -18,6 +19,7 @@ export default function AnswerPanel(props: { data: Record<string, Record<string,
       responses.forEach((response) => {
         const { id, correctAnswer } = response;
         if (correctAnswer) {
+          setCorrectValue(correctAnswer as string);
           for (const [user, answers] of Object.entries(data)) {
             const ans = answers[id];
             if (ans === correctAnswer) {
@@ -26,6 +28,24 @@ export default function AnswerPanel(props: { data: Record<string, Record<string,
               incorrect.push(user);
             }
           }
+          // set categorical data
+          if (response.type === 'radio') {
+            const map = new Map<string, number>();
+            for (const answers of Object.values(data)) {
+              const ans:string = answers[id] as string;
+              if (map.has(ans)) {
+                map.set(ans, map.get(ans) as number + 1);
+              } else {
+                map.set(ans, 1);
+              }
+            }
+
+            const categoryData:{option:string, count:number}[] = (response as RadioResponse).options.map((op) => ({
+              option: op.value as string,
+              count: map.get(op.value as string) || 0,
+            }));
+            setCategoricalStats(categoryData);
+          }
         }
       });
       setCorrectUser(correct);
@@ -33,7 +53,7 @@ export default function AnswerPanel(props: { data: Record<string, Record<string,
     }
   }, [data]);
 
-  const spec = {
+  const specBoxer = {
     data: {
       values: [
         { result: 'correct', start: 0, end: correctUser.length / (correctUser.length + incorrectUser.length) },
@@ -54,19 +74,32 @@ export default function AnswerPanel(props: { data: Record<string, Record<string,
     },
   };
 
+  const specBarChart = {
+    width: 300,
+    data: {
+      values: categoricalStats,
+    },
+    mark: { type: 'bar', cornerRadiusEnd: 5 },
+    encoding: {
+      x: { field: 'option', type: 'ordinal' },
+      y: { field: 'count', type: 'quantitative' },
+    },
+  };
+
   return (
     <Container p={10} sx={{ boxShadow: '1px 2px 2px 3px lightgrey;', borderRadius: '5px' }}>
-      <Group>
+      <Stack>
         <Box>
           {/* <CorrectVis correct={correctUser} incorrect={incorrectUser} trialName={trialName} /> */}
           {correctUser.length === 0 && incorrectUser.length === 0
             ? <Title order={4}> No correct answer for this question</Title>
-            : <VegaLite spec={spec} actions={false} />}
+            : <VegaLite spec={specBoxer} actions={false} />}
+
+          {correctValue !== '' && <VegaLite spec={specBarChart} actions={false} />}
 
         </Box>
         <Box />
-
-      </Group>
+      </Stack>
 
     </Container>
   );
