0e8f5cc904962eb23daf777e50bbb73fae43c630
Jack Wilburn <jackwilburn@tutanota.com>
2024-04-23 16:52:20 -0600

    Make test a bit more intense and cover all cases

--- tests/test-skip-logic.spec.ts
+++ tests/test-skip-logic.spec.ts
@@ -1,144 +1,194 @@
+/* eslint-disable no-await-in-loop */
 import { test, expect } from '@playwright/test';
+
+async function answerTrial1(page, q1, q2) {
+  await page.getByLabel(q1).check();
+  await page.getByLabel(q2).check();
+  await page.getByRole('button', { name: 'Next' }).click();
+}
+
+async function answerAttentionCheck(page, q1) {
+  await page.getByLabel(q1).check();
+  await page.getByRole('button', { name: 'Next' }).click();
+}
+
+async function answerAttentionCheckBlock(page, numIncorrect) {
+  const numCorrect = 3 - numIncorrect;
+  const answers = [...Array(numCorrect).fill('Yes'), ...Array(numIncorrect).fill('No')].sort(() => Math.random() - 0.5);
+
+  let stillToComplete = numIncorrect;
+  for (const answer of answers) {
+    await answerTrial1(page, 'Blue', 'Cat');
+    await answerAttentionCheck(page, answer);
+    if (answer === 'No') {
+      stillToComplete -= 1;
+    }
+
+    if (stillToComplete === 0 && numIncorrect !== 0) {
+      return;
+    }
+  }
+
+  await answerTrial1(page, 'Blue', 'Cat');
+}
+
+async function verifyContinuingComponent(page) {
+  await expect(page.getByText('This component exists to show that we didn\'t get skipped over.')).toBeVisible();
+  await page.getByRole('button', { name: 'Next' }).click();
+}
+
+async function verifyTargetComponent(page) {
+  await expect(page.getByText('This component exists to show that we can choose where to skip to.')).toBeVisible();
+  await page.getByRole('button', { name: 'Next' }).click();
+}
+
+async function verifyTargetBlockComponent(page) {
+  await expect(page.getByText('This component exists to show that we can choose a block to skip to.')).toBeVisible();
+  await page.getByRole('button', { name: 'Next' }).click();
+}
+
+async function verifyStudyEnd(page) {
+  await expect(page.getByText('Please wait while your answers are uploaded.')).toBeVisible();
+  await expect(page.getByText('Thank you for completing the study. You may close this window now.')).toBeVisible();
+}
+
+async function getNextParticipant(page) {
+  await page.locator('#mantine-r1-target').click();
+  await page.getByRole('menuitem', { name: 'Next Participant' }).click();
+}
+
+async function goToCheck(page, check: 'response' | 'responses' | 'attention-check-singular' | 'attention-check-block' | 'nested-responses' | 'nested-responses-block' | 'block-correct' | 'block-incorrect' | 'end') {
+  if (check === 'response') {
+    return;
+  }
+  await answerTrial1(page, 'Blue', 'Cat');
+  await verifyContinuingComponent(page);
+
+  if (check === 'responses') {
+    return;
+  }
+  await answerTrial1(page, 'Blue', 'Cat');
+  await verifyContinuingComponent(page);
+
+  if (check === 'attention-check-singular') {
+    return;
+  }
+  await answerAttentionCheck(page, 'Yes');
+  await answerTrial1(page, 'Blue', 'Cat');
+  await verifyContinuingComponent(page);
+
+  if (check === 'attention-check-block') {
+    return;
+  }
+
+  await answerAttentionCheckBlock(page, 0);
+  await verifyContinuingComponent(page);
+
+  if (check === 'nested-responses') {
+    return;
+  }
+  await verifyContinuingComponent(page);
+  await answerTrial1(page, 'Blue', 'Cat');
+  await verifyContinuingComponent(page);
+
+  await verifyContinuingComponent(page);
+
+  if (check === 'nested-responses-block') {
+    return;
+  }
+
+  await answerTrial1(page, 'Blue', 'Cat');
+  await answerTrial1(page, 'Blue', 'Cat');
+  await answerTrial1(page, 'Blue', 'Cat');
+  await verifyContinuingComponent(page);
+
+  await verifyContinuingComponent(page);
+
+  if (check === 'block-correct') {
+    return;
+  }
+  await answerTrial1(page, 'Red', 'Cat');
+  await answerTrial1(page, 'Red', 'Cat');
+  await answerTrial1(page, 'Red', 'Cat');
+
+  await verifyContinuingComponent(page);
+
+  if (check === 'block-incorrect') {
+    return;
+  }
+  await answerTrial1(page, 'Blue', 'Cat');
+  await answerTrial1(page, 'Blue', 'Cat');
+  await answerTrial1(page, 'Blue', 'Cat');
+
+  await verifyContinuingComponent(page);
+  await verifyTargetBlockComponent(page);
+  await verifyTargetComponent(page);
+  await verifyStudyEnd(page);
+
+  if (check === 'end') {
+    // eslint-disable-next-line no-useless-return
+    return;
+  }
+}
 
 test('test', async ({ page }) => {
   await page.goto('/test-skip-logic');
 
-  // ***** First participant, all questions are correct *****
-  // correct: q1 = Blue, q2 = any
-  // given: q1 = Blue, q2 = Cat
-  await page.getByLabel('Blue').check();
-  await page.getByLabel('Cat').check();
-  await page.getByRole('button', { name: 'Next' }).click();
+  // ***** All questions are correct *****
+  await goToCheck(page, 'end');
+  await getNextParticipant(page);
 
-  // continuing component
-  await expect(page.getByText('This component exists to show that we didn\'t get skipped over.')).toBeVisible();
-  await page.getByRole('button', { name: 'Next' }).click();
+  // ***** block-incorrect, block requires 2 incorrect to skip *****
+  await goToCheck(page, 'block-incorrect');
+  await answerTrial1(page, 'Blue', 'Dog'); // incorrect
+  await answerTrial1(page, 'Blue', 'Dog'); // incorrect
+  await verifyStudyEnd(page);
+  await getNextParticipant(page);
 
-  // correct: q1 = Blue, q2 = Cat
-  // given: q1 = Blue, q2 = Cat
-  await page.getByLabel('Blue').check();
-  await page.getByLabel('Cat').check();
-  await page.getByRole('button', { name: 'Next' }).click();
+  // ***** block-correct, block requires 2 correct to skip *****
+  await goToCheck(page, 'block-correct');
+  await answerTrial1(page, 'Blue', 'Cat'); // correct
+  await answerTrial1(page, 'Blue', 'Cat'); // correct
+  await verifyStudyEnd(page);
+  await getNextParticipant(page);
 
-  // continuing component
-  await expect(page.getByText('This component exists to show that we didn\'t get skipped over.')).toBeVisible();
-  await page.getByRole('button', { name: 'Next' }).click();
+  // ***** nested-responses, nested responses require 2 correct to skip *****
+  await goToCheck(page, 'nested-responses');
+  await verifyContinuingComponent(page);
+  await answerTrial1(page, 'Blue', 'Dog'); // incorrect
+  await verifyStudyEnd(page);
+  await getNextParticipant(page);
 
-  // correct: Yes
-  // given: Yes
-  await page.getByLabel('Yes').check();
-  await page.getByRole('button', { name: 'Next' }).click();
+  // ***** nested-responses-block, nested responses block requires 2 incorrect to skip *****
+  await goToCheck(page, 'nested-responses-block');
+  await answerTrial1(page, 'Blue', 'Dog'); // incorrect
+  await answerTrial1(page, 'Blue', 'Dog'); // incorrect
+  await verifyStudyEnd(page);
+  await getNextParticipant(page);
 
-  // correct: q1 = any, q2 = any
-  // given: q1 = Blue, q2 = Cat
-  await page.getByLabel('Blue').check();
-  await page.getByLabel('Cat').check();
-  await page.getByRole('button', { name: 'Next' }).click();
+  // ***** attention-check-block, attention check block requires 2 incorrect to skip *****
+  await goToCheck(page, 'attention-check-block');
+  await answerAttentionCheckBlock(page, 2);
+  await verifyTargetBlockComponent(page);
+  await verifyTargetComponent(page);
+  await verifyStudyEnd(page);
+  await getNextParticipant(page);
 
-  // continuing component
-  await expect(page.getByText('This component exists to show that we didn\'t get skipped over.')).toBeVisible();
-  await page.getByRole('button', { name: 'Next' }).click();
+  // ***** attention-check-singular, attention check requires incorrect to skip *****
+  await goToCheck(page, 'attention-check-singular');
+  await answerAttentionCheck(page, 'No'); // incorrect
+  await verifyStudyEnd(page);
+  await getNextParticipant(page);
 
-  // Study end
-  await expect(page.getByText('Please wait while your answers are uploaded.')).toBeVisible();
-  await expect(page.getByText('Thank you for completing the study. You may close this window now.')).toBeVisible();
+  // ***** responses, responses require incorrect to skip *****
+  await goToCheck(page, 'responses');
+  await answerTrial1(page, 'Blue', 'Dog'); // incorrect
+  await verifyStudyEnd(page);
+  await getNextParticipant(page);
 
-  // Next participant
-  await page.locator('#mantine-r1-target').click();
-  await page.getByRole('menuitem', { name: 'Next Participant' }).click();
-
-  // ***** Second participant, last question is incorrect (attention check) *****
-  // correct: q1 = Blue, q2 = any
-  // given: q1 = Blue, q2 = Cat
-  await page.getByLabel('Blue').check();
-  await page.getByLabel('Cat').check();
-  await page.getByRole('button', { name: 'Next' }).click();
-
-  // continuing component
-  await expect(page.getByText('This component exists to show that we didn\'t get skipped over.')).toBeVisible();
-  await page.getByRole('button', { name: 'Next' }).click();
-
-  // correct: q1 = Blue, q2 = Cat
-  // given: q1 = Blue, q2 = Cat
-  await page.getByLabel('Blue').check();
-  await page.getByLabel('Cat').check();
-  await page.getByRole('button', { name: 'Next' }).click();
-
-  // continuing component
-  await expect(page.getByText('This component exists to show that we didn\'t get skipped over.')).toBeVisible();
-  await page.getByRole('button', { name: 'Next' }).click();
-
-  // correct: Yes
-  // given: No
-  await page.getByLabel('No').check();
-  await page.getByRole('button', { name: 'Next' }).click();
-
-  // Study end
-  await expect(page.getByText('Please wait while your answers are uploaded.')).toBeVisible();
-  await expect(page.getByText('Thank you for completing the study. You may close this window now.')).toBeVisible();
-
-  // Next participant
-  await page.locator('#mantine-r1-target').click();
-  await page.getByRole('menuitem', { name: 'Next Participant' }).click();
-
-  // ***** Third participant, test all responses (1 incorrect triggers skip) *****
-  // correct: q1 = Blue, q2 = any
-  // given: q1 = Blue, q2 = Cat
-  await page.getByLabel('Blue').check();
-  await page.getByLabel('Cat').check();
-  await page.getByRole('button', { name: 'Next' }).click();
-
-  // continuing component
-  await expect(page.getByText('This component exists to show that we didn\'t get skipped over.')).toBeVisible();
-  await page.getByRole('button', { name: 'Next' }).click();
-
-  // correct: q1 = Blue, q2 = Cat
-  // given: q1 = Blue, q2 = Dog
-  await page.getByLabel('Blue').check();
-  await page.getByLabel('Dog').check();
-  await page.getByRole('button', { name: 'Next' }).click();
-
-  // Study end
-  await expect(page.getByText('Please wait while your answers are uploaded.')).toBeVisible();
-  await expect(page.getByText('Thank you for completing the study. You may close this window now.')).toBeVisible();
-
-  // Next participant
-  await page.locator('#mantine-r1-target').click();
-  await page.getByRole('menuitem', { name: 'Next Participant' }).click();
-
-  // ***** Fourth participant, test all responses (All incorrect triggers skip) *****
-  // correct: q1 = Blue, q2 = any
-  // given: q1 = Blue, q2 = Cat
-  await page.getByLabel('Blue').check();
-  await page.getByLabel('Cat').check();
-  await page.getByRole('button', { name: 'Next' }).click();
-
-  // continuing component
-  await expect(page.getByText('This component exists to show that we didn\'t get skipped over.')).toBeVisible();
-  await page.getByRole('button', { name: 'Next' }).click();
-
-  // correct: q1 = Blue, q2 = Cat
-  // given: q1 = Blue, q2 = Dog
-  await page.getByLabel('Red').check();
-  await page.getByLabel('Dog').check();
-  await page.getByRole('button', { name: 'Next' }).click();
-
-  // Study end
-  await expect(page.getByText('Please wait while your answers are uploaded.')).toBeVisible();
-  await expect(page.getByText('Thank you for completing the study. You may close this window now.')).toBeVisible();
-
-  // Next participant
-  await page.locator('#mantine-r1-target').click();
-  await page.getByRole('menuitem', { name: 'Next Participant' }).click();
-
-  // ***** Fifth participant, test response incorrect skips *****
-  // correct: q1 = Blue, q2 = any
-  // given: q1 = Red, q2 = Cat
-  await page.getByLabel('Red').check();
-  await page.getByLabel('Cat').check();
-  await page.getByRole('button', { name: 'Next' }).click();
-
-  // Study end
-  await expect(page.getByText('Please wait while your answers are uploaded.')).toBeVisible();
-  await expect(page.getByText('Thank you for completing the study. You may close this window now.')).toBeVisible();
+  // ***** response, no must answer part of answer incorrect *****
+  await goToCheck(page, 'response');
+  await answerTrial1(page, 'Red', 'Cat'); // incorrect
+  await verifyTargetComponent(page);
+  await verifyStudyEnd(page);
 });
