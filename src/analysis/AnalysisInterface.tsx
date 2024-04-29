d3ab18e901f730d2b42a1f6b16acf7083eb91adf
Brian Bollen <briancbollen@gmail.com>
2024-04-29 11:14:18 -0400

    Moved settings page

--- src/analysis/AnalysisInterface.tsx
+++ src/analysis/AnalysisInterface.tsx
@@ -5,7 +5,6 @@ import AppHeader from './components/interface/AppHeader';
 import AppNav from './components/interface/AppNav';
 import { GlobalConfig } from '../parser/types';
 import { SummaryBlock } from './dashboard/SummaryBlock';
-import { GlobalSettings } from './dashboard/GlobalSettings';
 
 export function AnalysisInterface(props: { globalConfig: GlobalConfig; }) {
   const { page } = useParams();
@@ -17,9 +16,6 @@ export function AnalysisInterface(props: { globalConfig: GlobalConfig; }) {
       <Container fluid>
         {page === 'dashboard' && (
           <SummaryBlock globalConfig={props.globalConfig} />
-        )}
-        {page === 'settings' && (
-          <GlobalSettings />
         )}
       </Container>
     </AppShell>
