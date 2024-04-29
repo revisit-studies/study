18cfaabc52cb185f673a3c4c458f53558082e6f0
Yiren Ding <dingx315@hotmail.com>
2024-04-29 10:58:07 -0400

    update table interface

--- src/analysis/stats/TableView.tsx
+++ src/analysis/stats/TableView.tsx
@@ -1,9 +1,8 @@
 import {
-  Box, Spoiler, Stack, Table, Title,
+  Box, Spoiler, Stack, Table, Title, Text,
 } from '@mantine/core';
-import { Metadata } from '@trrack/core';
 import { IconCheck, IconProgress } from '@tabler/icons-react';
-import { GlobalConfig, ParticipantData, StoredAnswer } from '../../parser/types';
+import { ParticipantData, StoredAnswer } from '../../parser/types';
 import { flattenSequence } from '../utils';
 import { ParticipantMetadata } from '../../store/types';
 
@@ -14,12 +13,26 @@ function TableCell(props: {cellData: StoredAnswer}) {
     <td>
       <Stack miw={100}>
         <Box>
-          Duration:
+          <Text fw={700} span>Duration:</Text>
           {duration.toFixed(1)}
           {' '}
           s
         </Box>
-        {Object.entries(cellData.answer).map(([key, storedAnswer]) => <Box key={`cell-${key}`}>{`${key}:${storedAnswer}`}</Box>)}
+        {Object.entries(cellData.answer).map(([key, storedAnswer]) => (
+          <Box key={`cell-${key}`}>
+            <Text fw={500} span>
+              <Text fw={700} span>
+                {' '}
+                {`${key}: `}
+              </Text>
+
+            </Text>
+            <Text span>
+              {`${storedAnswer}`}
+            </Text>
+
+          </Box>
+        ))}
       </Stack>
     </td>
   );
@@ -53,7 +66,6 @@ function MetaCell(props:{metaData: ParticipantMetadata}) {
           </Box>
         </Stack>
       </Spoiler>
-
     </td>
   );
 }
@@ -64,13 +76,12 @@ export function TableView(props: { completed: ParticipantData[], inprogress: Par
 
   const uniqueTrials = [...new Set(completed.map((complete) => flattenSequence(complete.sequence)).flat().map((trial) => trial))];
 
-  const headers = [<th key="ID">ID</th>, <th key="meta">Meta</th>, ...uniqueTrials.map((trialName) => <th key={`header-${trialName}`}>{trialName}</th>)];
+  const headers = [<th key="ID">ID/Status</th>, <th key="meta">Meta</th>, ...uniqueTrials.map((trialName) => <th key={`header-${trialName}`}>{trialName}</th>)];
   const rows = allData.map((record) => (
     <tr key={record.participantId}>
       <td>
-        <Box>
+        <Box sx={{ display: 'block', whiteSpace: 'nowrap' }}>
           {record.participantId}
-          {' '}
           {record.completed ? <IconCheck size={15} color="teal" /> : <IconProgress size={15} color="orange" />}
         </Box>
       </td>
@@ -85,9 +96,7 @@ export function TableView(props: { completed: ParticipantData[], inprogress: Par
     completed.length > 0 || inprogress.length > 0 ? (
       <Table>
         <thead>
-          <tr>
-            {headers}
-          </tr>
+          <tr>{headers}</tr>
         </thead>
         <tbody>{rows}</tbody>
       </Table>
