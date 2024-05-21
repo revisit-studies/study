ca6a37f0d09991c82802ebdcc59d988313b950f6
ZachCutler04 <zach.t.cutler@gmail.com>
2024-05-21 13:24:05 -0600

    fixing slider error in brushplot study

--- src/public/demo-brush-interactions/assets/Scatter.tsx
+++ src/public/demo-brush-interactions/assets/Scatter.tsx
@@ -149,6 +149,10 @@ export function Scatter({
       };
     }
     if (brushType === 'Slider Selection') {
+      if (!brushState.hasBrush) {
+        setBrushedSpace([[xScale(xMin), yScale(yMax)], [xScale(xMax), yScale(yMin)]], xScale, yScale, null);
+      }
+
       return () => setBrushedSpace([[xScale(xMin), yScale(yMax)], [xScale(xMax), yScale(yMin)]], xScale, yScale, 'clear');
     }
     if (brushType === 'Paintbrush Selection') {
@@ -158,11 +162,10 @@ export function Scatter({
     return () => null;
   }, [brushState.hasBrush, brushType, brushXRef, brushYRef, height, ref, setBrushedSpace, setFilteredTable, width, xMax, xMin, xScale, yMax, yMin, yScale]);
 
-  useEffect(() => {
-    if (brushType === 'Slider Selection' && xScale && yScale) {
-      setBrushedSpace([[xScale(xMin), yScale(yMax)], [xScale(xMax), yScale(yMin)]], xScale, yScale, null);
-    }
-  }, [brushType, setBrushedSpace, xMax, xMin, xScale, yMax, yMin, yScale]);
+  // useEffect(() => {
+  //   if (brushType === 'Slider Selection' && xScale && yScale) {
+  //   }
+  // }, [brushType, setBrushedSpace, xMax, xMin, xScale, yMax, yMin, yScale]);
 
   const brushedSet = useMemo(() => (brushedPoints.length === 0 ? null : new Set(brushedPoints)), [brushedPoints]);
 
