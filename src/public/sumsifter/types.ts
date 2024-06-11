1b16996b83d4386cdaf21fd88694deb69ce81a8c
Lane Harrison <ltharrison@wpi.edu>
2024-06-11 13:49:02 -0400

    adds starter csv data load

---
+++ src/public/sumsifter/types.ts
@@ -0,0 +1,17 @@
+export interface BrushState {
+    hasBrush: boolean;
+    x1: number;
+    x2: number;
+    y1: number;
+    y2: number;
+
+    ids: string[];
+  }
+
+export type SelectionType = 'drag' | 'handle' | 'clear' | null
+
+export type BrushNames = 'Rectangular Selection' | 'Axis Selection' | 'Slider Selection' | 'Paintbrush Selection'
+
+export interface BrushParams {brushType: BrushNames, dataset: string, x: string, y: string, category: string, ids: string, dataType?: 'date'}
+
+export interface SumParams {datasetSource: string, datasetSummary: string}
