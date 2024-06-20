e0cc6d684939f01ba551cf925dd474a2a85bc97f
Brian Bollen <briancbollen@gmail.com>
2024-06-20 12:43:27 -0600

    Added protected routes to individual analysis interfaces dependent on revisit mode.

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,42 +1,53 @@
 import { LoadingOverlay } from '@mantine/core';
-import { ReactNode, useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
+import { ReactNode, useEffect, useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
 import { useAuth } from './store/hooks/useAuth';
 import { useStorageEngine } from './storage/storageEngineHooks';
 
 interface ProtectedRouteProps {
   children: ReactNode;
+  paramToCheck?: string;
+  paramCallback?: (paramToCheck:string) => Promise<boolean>;
 }
 
 // Wrapper component which only allows users who are authenticated and admins to access its child components.
-export function ProtectedRoute({ children }: ProtectedRouteProps) {
+export function ProtectedRoute({ children, paramToCheck, paramCallback }: ProtectedRouteProps) {
   const { user, verifyAdminStatus, logout } = useAuth();
   const { storageEngine } = useStorageEngine();
+  const [isEnabled, setIsEnabled] = useState<boolean>(false);
+  const params = useParams();
 
   useEffect(() => {
     const verifyUser = async () => {
-      try {
-        if (user) {
-          const isAdmin = await verifyAdminStatus(user);
-          if (!isAdmin) {
+      if (paramToCheck && paramCallback && params[paramToCheck] && isEnabled === false) {
+        const currIsEnabled = await paramCallback(params[paramToCheck]!);
+        setIsEnabled(currIsEnabled);
+      }
+      if (isEnabled) {
+        try {
+          if (user) {
+            const isAdmin = await verifyAdminStatus(user);
+            if (!isAdmin) {
+              logout();
+            }
+          } else {
             logout();
           }
-        } else {
+        } catch (error) {
           logout();
+          console.warn(error);
         }
-      } catch (error) {
-        logout();
-        console.warn(error);
       }
     };
+
     verifyUser();
-  }, [user.isAdmin]);
+  }, [user.isAdmin, isEnabled]);
 
   if (user.determiningStatus || !storageEngine) {
     return <LoadingOverlay visible={user.determiningStatus || !storageEngine?.getEngine()} />;
   }
 
-  if (!user.isAdmin && !user.determiningStatus) {
+  if (isEnabled && !user.isAdmin && !user.determiningStatus) {
     return <Navigate to="/login" />;
   }
   // eslint-disable-next-line react/jsx-no-useless-fragment
