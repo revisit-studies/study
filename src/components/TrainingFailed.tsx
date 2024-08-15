e1da71b63596bd00305cd2b968732e1aded20a16
Jack Wilburn <jackwilburn@tutanota.com>
2024-08-15 17:03:41 -0600

    Change wording

--- src/components/TrainingFailed.tsx
+++ src/components/TrainingFailed.tsx
@@ -3,7 +3,7 @@ import { Text } from '@mantine/core';
 export function TrainingFailed() {
   return (
     <Text>
-      Thank you for participating. Unfortunately you have failed the training, which means you are not eligible to participate in the study. You may close this window now.
+      Thank you for participating. Unfortunately you have didn&apos;t answer the training correctly, which means you are not eligible to participate in the study. You may close this window now.
     </Text>
   );
 }
