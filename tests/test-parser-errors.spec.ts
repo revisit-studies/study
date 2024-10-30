3da9e174acdbc9e300ee5dbc53b401848a61a6e4
Jack Wilburn <jackwilburn@tutanota.com>
2024-10-30 16:09:54 -0600

    Increase parser test coverage for library errors/warnings

--- tests/test-parser-errors.spec.ts
+++ tests/test-parser-errors.spec.ts
@@ -3,18 +3,36 @@ import { expect, test } from '@playwright/test';
 test('parser errors are shown correctly', async ({ page }) => {
   await page.goto('/');
 
-  const e1 = await page.getByText('You have an error at /components/missingBaseComponent: Base component missingBaseComponent is not defined in baseComponents object - {"action":"add the base component to the baseComponents object"}');
-  await expect(e1).toBeVisible();
+  const errors = [
+    'You have an error at /importedLibraries/: Could not find library test-missing - {"action":"make sure the library is in the correct location"}',
+    'You have an error at /components/missingBaseComponent: Base component missingBaseComponent is not defined in baseComponents object - {"action":"add the base component to the baseComponents object"}',
+    'You have an error at /components/missingComponentFromLibrary: Base component $test.co.test-missing is not defined in baseComponents object - {"action":"add the base component to the baseComponents object"}',
+    'You have an error at /sequence/: Component missingComponent is not defined in components object - {"action":"add the component to the components object"}',
+    'You have an error at /sequence/: Component testBaseComponent is a base component and cannot be used in the sequence - {"action":"add the component to the components object"}',
+    'You have an error at /sequence/: Component nestedMissingComponent is not defined in components object - {"action":"add the component to the components object"}',
+    'You have an error at /sequence/: Skip target md1 does not occur after the skip block it is used in - {"action":"add the target to the sequence after the skip block"}',
+  ];
 
-  const e2 = await page.getByText('You have an error at /sequence/: Component missingComponent is not defined in components object - {"action":"add the component to the components object"}');
-  await expect(e2).toBeVisible();
+  for (const error of errors) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    const e = page.getByText(error);
+    // eslint-disable-next-line no-await-in-loop
+    await expect(e).toBeVisible();
+  }
 
-  const e3 = await page.getByText('You have an error at /sequence/: Component testBaseComponent is a base component and cannot be used in the sequence - {"action":"add the component to the components object"}');
-  await expect(e3).toBeVisible();
+  const warnings = [
+    'Component testComponent is defined in components object but not used in the sequence - {"action":"remove the component from the components object or add it to the sequence"}',
+  ];
 
-  const e4 = await page.getByText('You have an error at /sequence/: Component nestedMissingComponent is not defined in components object - {"action":"add the component to the components object"}');
-  await expect(e4).toBeVisible();
+  for (const warning of warnings) {
+    // eslint-disable-next-line no-console
+    console.log(warning);
+    const w = page.getByText(warning);
+    // eslint-disable-next-line no-await-in-loop
+    await expect(w).toBeVisible();
+  }
 
-  const e5 = await page.getByText('You have an error at /sequence/: Skip target md1 does not occur after the skip block it is used in - {"action":"add the target to the sequence after the skip block"}');
-  await expect(e5).toBeVisible();
+  const count = await page.getByRole('button', { name: 'test-parser-errors Errors' }).getByRole('list').locator('li').count();
+  expect(count).toBe(errors.length + warnings.length);
 });
