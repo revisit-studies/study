19943c70f80c4bc7bf11fc1085043c6d43445f27
Brian Bollen <briancbollen@gmail.com>
2024-12-06 17:45:37 -0700

    Moved radio labels to above buttons. Stretched likrt to span width.

--- src/components/response/RadioInput.tsx
+++ src/components/response/RadioInput.tsx
@@ -30,35 +30,54 @@ export default function RadioInput({
   const optionsAsStringOptions = options.map((option) => (typeof option === 'string' ? { value: option, label: option } : option));
 
   return (
-    <Radio.Group
-      name={`radioInput${response.id}`}
-      label={(
-        <Flex direction="row" wrap="nowrap" gap={4}>
-          {enumerateQuestions && <Box style={{ minWidth: 'fit-content' }}>{`${index}. `}</Box>}
-          <Box style={{ display: 'block' }} className="no-last-child-bottom-padding">
-            <ReactMarkdownWrapper text={prompt} required={required} />
-          </Box>
-        </Flex>
-      )}
-      description={secondaryText}
-      key={response.id}
-      {...answer}
-        // This overrides the answers error. Which..is bad?
-      error={generateErrorMessage(response, answer, optionsAsStringOptions)}
-      style={{ '--input-description-size': 'calc(var(--mantine-font-size-md) - calc(0.125rem * var(--mantine-scale)))' }}
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+    }}
     >
-      <Group mt="xs">
-        {leftLabel ? <Text style={{ textAlign: 'center' }}>{leftLabel}</Text> : null}
-        {optionsAsStringOptions.map((radio) => (
-          <Radio
-            disabled={disabled}
-            value={radio.value}
-            label={radio.label}
-            key={radio.label}
-          />
-        ))}
-        <Text>{rightLabel}</Text>
-      </Group>
-    </Radio.Group>
+      <div />
+      <Radio.Group
+        name={`radioInput${response.id}`}
+        label={(
+          <Flex direction="row" wrap="nowrap" gap={4}>
+            {enumerateQuestions && <Box style={{ minWidth: 'fit-content' }}>{`${index}. `}</Box>}
+            <Box style={{ display: 'block' }} className="no-last-child-bottom-padding">
+              <ReactMarkdownWrapper text={prompt} required={required} />
+            </Box>
+          </Flex>
+        )}
+        description={secondaryText}
+        key={response.id}
+        {...answer}
+        // This overrides the answers error. Which..is bad?
+        error={generateErrorMessage(response, answer, optionsAsStringOptions)}
+        style={{ '--input-description-size': 'calc(var(--mantine-font-size-md) - calc(0.125rem * var(--mantine-scale)))' }}
+      >
+        {/*
+        Optional: If we do not like the horizontal spread, we can remove the 'space-around'.
+         */}
+        <Group mt="md" style={{ display: 'flex', alignItems: 'flex-end', justifyContent: 'space-around' }}>
+          {leftLabel ? <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>{leftLabel}</Text> : null}
+          {optionsAsStringOptions.map((radio) => (
+            <div
+              key={radio.label}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Text>{radio.label}</Text>
+              <Radio
+                disabled={disabled}
+                value={radio.value}
+              />
+            </div>
+          ))}
+          <Text style={{ fontWeight: 'bold' }}>{rightLabel}</Text>
+        </Group>
+      </Radio.Group>
+    </div>
+
   );
 }
