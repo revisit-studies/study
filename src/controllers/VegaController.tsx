ae881c701f42ba578b5049a61eb95760ec2be25a
Hilson Shrestha <hilsonshrestha@gmail.com>
2024-12-12 21:02:36 -0500

    Add vega demo and update answer with interaction

--- src/controllers/VegaController.tsx
+++ src/controllers/VegaController.tsx
@@ -1,23 +1,63 @@
-import { useEffect, useState } from 'react';
-import { VegaLite } from 'react-vega';
+import {
+  useCallback, useEffect, useMemo, useState,
+} from 'react';
+import { Vega, VisualizationSpec } from 'react-vega';
 import { VegaComponent } from '../parser/types';
-// import { PREFIX } from '../utils/Prefix';
 import { getJsonAssetByPath } from '../utils/getStaticAsset';
 import ResourceNotFound from '../ResourceNotFound';
+import { useStoreActions, useStoreDispatch } from '../store/store';
+import { StimulusParams } from '../store/types';
+import { useCurrentComponent, useCurrentStep } from '../routes/utils';
 
-// const defaultStyle = {
-//   maxWidth: '100%',
-// };
+function VegaController({ currentConfig }: { currentConfig: VegaComponent }) {
+  const currentStep = useCurrentStep();
+  const currentComponent = useCurrentComponent();
+  const storeDispatch = useStoreDispatch();
 
-function VegaController({ currentConfig }: { currentConfig: VegaComponent; }) {
   const [loading, setLoading] = useState(true);
-  const [assetFound, setAssetFound] = useState(false);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [vegaConfig, setVegaConfig] = useState<Record<string, any>>({});
+
+  const { updateResponseBlockValidation, setIframeAnswers } = useStoreActions();
+
+  const setAnswer = useCallback(({
+    status,
+    provenanceGraph,
+    answers,
+  }: Parameters<StimulusParams<unknown>['setAnswer']>[0]) => {
+    storeDispatch(
+      updateResponseBlockValidation({
+        location: 'sidebar',
+        identifier: `${currentComponent}_${currentStep}`,
+        status,
+        values: answers,
+        provenanceGraph,
+      }),
+    );
+
+    storeDispatch(setIframeAnswers(answers));
+  }, [currentComponent, currentStep, storeDispatch, setIframeAnswers, updateResponseBlockValidation]);
+
+  const handleRevisitAnswer = useCallback((key: string, value: unknown) => {
+    const { responseId, response } = value as {responseId: string, response: unknown};
+    setAnswer({
+      status: true,
+      provenanceGraph: undefined,
+      answers: {
+        [responseId]: response,
+      },
+    });
+  }, [setAnswer]);
+
+  const signalListeners = useMemo(
+    () => ({ revisitAnswer: handleRevisitAnswer }),
+    [handleRevisitAnswer],
+  );
+
+  const [vegaConfig, setVegaConfig] = useState<VisualizationSpec | null>(null);
+
   useEffect(() => {
     async function fetchVega() {
+      setLoading(true);
       const asset = await getJsonAssetByPath(currentConfig.path);
-      setAssetFound(!!asset);
       if (asset !== undefined) {
         setVegaConfig(asset);
       }
@@ -27,10 +67,14 @@ function VegaController({ currentConfig }: { currentConfig: VegaComponent; }) {
     fetchVega();
   }, [currentConfig]);
 
-  return loading || assetFound ? (
-    <VegaLite spec={vegaConfig.spec || {}} data={vegaConfig.data || {}} />
-  )
-    : <ResourceNotFound path={currentConfig.path} />;
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (!vegaConfig) {
+    return <ResourceNotFound path={currentConfig.path} />;
+  }
+
+  return (<Vega spec={vegaConfig} signalListeners={signalListeners} />);
 }
 
 export default VegaController;
