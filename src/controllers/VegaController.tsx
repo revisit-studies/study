ecbb28bb4a5c6bf893dfd0acacf3d8834acd937f
Alex Dean <dingx315@hotmail.com>
2024-12-15 16:45:22 -0800

    track all signals from vega config

--- src/controllers/VegaController.tsx
+++ src/controllers/VegaController.tsx
@@ -2,6 +2,7 @@ import {
   useCallback, useEffect, useMemo, useState,
 } from 'react';
 import { Vega, VisualizationSpec } from 'react-vega';
+import { initializeTrrack, Registry } from '@trrack/core';
 import { VegaComponent } from '../parser/types';
 import { getJsonAssetByPath } from '../utils/getStaticAsset';
 import ResourceNotFound from '../ResourceNotFound';
@@ -13,10 +14,32 @@ function VegaController({ currentConfig }: { currentConfig: VegaComponent }) {
   const currentStep = useCurrentStep();
   const currentComponent = useCurrentComponent();
   const storeDispatch = useStoreDispatch();
-
+  const [vegaConfig, setVegaConfig] = useState<VisualizationSpec | null>(null);
   const [loading, setLoading] = useState(true);
-
   const { updateResponseBlockValidation, setIframeAnswers } = useStoreActions();
+
+  const { actions, trrack } = useMemo(() => {
+    const reg = Registry.create();
+
+    const signalAction = reg.register('signal', (state, signalEvt) => {
+      state.event = signalEvt;
+      return state;
+    });
+
+    const trrackInst = initializeTrrack({
+      registry: reg,
+      initialState: {
+        event: '',
+      },
+    });
+
+    return {
+      actions: {
+        signalAction,
+      },
+      trrack: trrackInst,
+    };
+  }, []);
 
   const setAnswer = useCallback(({
     status,
@@ -36,23 +59,36 @@ function VegaController({ currentConfig }: { currentConfig: VegaComponent }) {
     storeDispatch(setIframeAnswers(answers));
   }, [currentComponent, currentStep, storeDispatch, setIframeAnswers, updateResponseBlockValidation]);
 
+  const handleSignalEvt = useCallback((key: string) => {
+    trrack.apply(key, actions.signalAction(key));
+  }, []);
+
   const handleRevisitAnswer = useCallback((key: string, value: unknown) => {
     const { responseId, response } = value as {responseId: string, response: unknown};
+
     setAnswer({
       status: true,
-      provenanceGraph: undefined,
+      provenanceGraph: trrack.graph.backend,
       answers: {
         [responseId]: response,
       },
     });
   }, [setAnswer]);
 
-  const signalListeners = useMemo(
-    () => ({ revisitAnswer: handleRevisitAnswer }),
-    [handleRevisitAnswer],
-  );
-
-  const [vegaConfig, setVegaConfig] = useState<VisualizationSpec | null>(null);
+  const signalListeners = useMemo(() => {
+    const signals = vegaConfig?.config?.signals;
+    if (!signals) return {};
+    type Listeners = { [key: string]: (key: string, value: unknown) => void };
+    const listenerList = signals.reduce((listeners, signal) => {
+      if (signal.name === 'revisitAnswer') {
+        listeners[signal.name] = handleRevisitAnswer;
+      } else {
+        listeners[signal.name] = handleSignalEvt;
+      }
+      return listeners;
+    }, {} as Listeners);
+    return listenerList;
+  }, [handleRevisitAnswer, handleSignalEvt, vegaConfig]);
 
   useEffect(() => {
     async function fetchVega() {
