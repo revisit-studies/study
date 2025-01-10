51346672067500bafdfb52532c54035e24c1d739
Jack Wilburn <jackwilburn@tutanota.com>
2025-01-10 14:19:54 -0500

    Fix some tests

--- tests/test-training-feeback.spec.ts
+++ tests/test-training-feeback.spec.ts
@@ -30,7 +30,7 @@ async function goToTraining(page) {
 
 test('test', async ({ page }) => {
   await page.goto('/');
-  await page.getByRole('tab', { name: 'Tests' }).click();
+  await page.getByRole('tab', { name: 'Example Studies' }).click();
 
   // Click on cleveland
   await page.getByRole('button', { name: 'Dynamic React.js Stimuli: A Graphical Perception Experiment' }).click();
