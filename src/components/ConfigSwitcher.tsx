3ddf80718b574d7bdcff962be83f7c8c3db77002
Jack Wilburn <jackwilburn@tutanota.com>
2025-01-23 12:55:01 -0700

    Don't navigate with PREFIX

--- src/components/ConfigSwitcher.tsx
+++ src/components/ConfigSwitcher.tsx
@@ -221,7 +221,7 @@ export function ConfigSwitcher({
           src={`${PREFIX}revisitAssets/revisitLogoSquare.svg`}
           alt="reVISit"
         />
-        <Tabs variant="outline" defaultValue={others.length > 0 ? 'Others' : 'Demos'} value={tab} onChange={(value) => navigate(`${PREFIX}?tab=${value}`)}>
+        <Tabs variant="outline" defaultValue={others.length > 0 ? 'Others' : 'Demos'} value={tab} onChange={(value) => navigate(`/?tab=${value}`)}>
           <Tabs.List>
             {others.length > 0 && (
               <Tabs.Tab value="Others">Your Studies</Tabs.Tab>
