e2199b11fab102bc145bf383d5656dbc1fd67e61
abrar <abrar.nadib@gmail.com>
2025-02-27 11:53:42 -0700

    Fix practice result text sequence parallel

--- src/public/jnd-replication/vistaJND/src/components/vis/ParallelCoordinatesWrapper.tsx
+++ src/public/jnd-replication/vistaJND/src/components/vis/ParallelCoordinatesWrapper.tsx
@@ -15,9 +15,9 @@ import ParallelCoordinates from './ParallelCoordinates';
  * @returns 2 Parallel Coordinate Plots
  */
 export default function ParallelCoordinatesWrapper({
-  r1, r2, shouldReRender = true, onClick,
-}: {r1: number; r2: number, shouldReRender?: boolean, onClick: (n: number, higherFirst?: boolean) => void}) {
-  const higherFirst = useMemo(() => Math.random() > 0.5, []);
+  r1, r2, shouldReRender = true, onClick, shouldRandomize = true,
+}: {r1: number; r2: number, shouldReRender?: boolean, onClick: (n: number) => void, shouldRandomize?: boolean}) {
+  const higherFirst = useMemo(() => (shouldRandomize ? Math.random() > 0.5 : true), [shouldRandomize]);
 
   const [key, setKey] = useState<number>(0);
 
@@ -29,7 +29,7 @@ export default function ParallelCoordinatesWrapper({
   };
 
   const handleClick = (n: number) => {
-    onClick(n, higherFirst);
+    onClick(n);
     handleReset();
   };
 
