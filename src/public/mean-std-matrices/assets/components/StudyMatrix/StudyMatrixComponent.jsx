6f298865ab8f879825054fdbb525bd24044686f4
Jorge <jorge.acosta@upm.es>
2025-03-27 18:47:59 -0600

    connected to firebase!

--- src/public/mean-std-matrices/assets/components/StudyMatrix/StudyMatrixComponent.jsx
+++ src/public/mean-std-matrices/assets/components/StudyMatrix/StudyMatrixComponent.jsx
@@ -1,27 +1,19 @@
-import React, { useEffect, useState, useRef } from "react";
-import D3Matrix from "./D3Matrix/index.js";
-import "./style.css";
-import { useDispatch, useSelector } from "react-redux";
-import useResizeObserver from "./useResizeObserver";
-import Menu from "../Menu";
-import { setParameters } from "../../features/matrix/matrixSlice";
-import { Button, Loader } from "@mantine/core";
+import React, { useEffect, useState, useRef } from 'react';
+import D3Matrix from './D3Matrix/index.js';
+import './style.css';
+import { useDispatch, useSelector } from 'react-redux';
+import useResizeObserver from './useResizeObserver.js';
+import Menu from '../Menu/index.jsx';
+import { setParameters } from '../../features/matrix/matrixSlice.js';
+import { Button, Loader } from '@mantine/core';
 
 const StudyMatrixComponent = ({ parameters, setAnswer, provenanceState }) => {
   const { data, loading } = useLoadData(parameters.file);
 
   return data ? (
-    <StudyMatrix
-      data={data}
-      parameters={parameters}
-      setAnswer={setAnswer}
-      provenanceState={provenanceState}
-    ></StudyMatrix>
+    <StudyMatrix data={data} parameters={parameters} setAnswer={setAnswer} provenanceState={provenanceState}></StudyMatrix>
   ) : (
-    <Loader
-      size={50}
-      style={{ position: "absolute", top: "50%", left: "50%" }}
-    />
+    <Loader size={50} style={{ position: 'absolute', top: '50%', left: '50%' }} />
   );
 };
 
@@ -37,7 +29,7 @@ const StudyMatrix = ({ data, parameters, setAnswer, provenanceState }) => {
 
     return () => {
       // should include a better cleanup...
-      if (matrixRef.current) matrixRef.current.innerHTML = "";
+      if (matrixRef.current) matrixRef.current.innerHTML = '';
     };
   }, []);
 
@@ -67,15 +59,15 @@ const StudyButtons = () => {
     <div id="buttons-div">
       <div
         style={{
-          display: "flex",
-          gap: "5px",
+          display: 'flex',
+          gap: '5px',
         }}
       >
         <div
           style={{
-            display: "flex",
-            gap: "5px",
-            flexDirection: "column",
+            display: 'flex',
+            gap: '5px',
+            flexDirection: 'column',
           }}
         >
           <Button id="mean-optimal-clustering">Optimal Leaf Clustering</Button>
@@ -83,26 +75,22 @@ const StudyButtons = () => {
         </div>
         <div
           style={{
-            display: "flex",
-            gap: "5px",
-            flexDirection: "column",
+            display: 'flex',
+            gap: '5px',
+            flexDirection: 'column',
           }}
         >
-          <Button id="snr-optimal-clustering">
-            Optimal Leaf Clustering (snr)
-          </Button>
+          <Button id="snr-optimal-clustering">Optimal Leaf Clustering (snr)</Button>
           <Button id="snr-pca-clustering">PCA Clustering (snr)</Button>
         </div>
         <div
           style={{
-            display: "flex",
-            gap: "5px",
-            flexDirection: "column",
+            display: 'flex',
+            gap: '5px',
+            flexDirection: 'column',
           }}
         >
-          <Button id="std-optimal-clustering">
-            Optimal Leaf Clustering (std)
-          </Button>
+          <Button id="std-optimal-clustering">Optimal Leaf Clustering (std)</Button>
           <Button id="std-pca-clustering">PCA Clustering (std)</Button>
         </div>
       </div>
@@ -182,17 +170,17 @@ async function loadData(file, setData, setLoading) {
   setLoading(true);
   try {
     const response = await fetch(`data/${file}`);
-    if (!response.ok) throw new Error("Failed to fetch file");
+    if (!response.ok) throw new Error('Failed to fetch file');
 
     const text = await response.text();
     const parsedData = text
-      .split("\n")
+      .split('\n')
       .filter((line) => line.trim().length > 0)
       .map((line) => JSON.parse(line));
-    console.log("NEW DATA", parsedData);
+    console.log('NEW DATA', parsedData);
     setData(parsedData);
   } catch (error) {
-    console.error("Error loading JSON:", error);
+    console.error('Error loading JSON:', error);
   } finally {
     setLoading(false);
   }
