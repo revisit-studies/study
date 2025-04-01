5512911fb283546b344ee5767441a85d44b7ac3a
Yeon <76601570+yeonkim1213@users.noreply.github.com>
2025-04-01 14:41:00 -0600

    Add dynamic block demo with react component

---
+++ src/public/demo-dynamic/assets/HSL.tsx
@@ -0,0 +1,11 @@
+import { Flex } from '@mantine/core';
+import { StimulusParams } from '../../../store/types';
+
+export default function HSL({ parameters }: StimulusParams<{ left: number; right: number }>) {
+  return (
+    <Flex direction="row" justify="space-between">
+      <div style={{ width: '40%', height: '150px', backgroundColor: `hsl(248, ${parameters.left}%, 50%)` }} />
+      <div style={{ width: '40%', height: '150px', backgroundColor: `hsl(248, ${parameters.right}%, 50%)` }} />
+    </Flex>
+  );
+}
---
+++ src/public/demo-dynamic/assets/dynamic.tsx
@@ -0,0 +1,10 @@
+// import { JumpFunctionParameters, JumpFunctionReturnVal } from '../../../store/types';
+
+// export default function dynamic({ answers }: JumpFunctionParameters<never>): JumpFunctionReturnVal {
+//   // Check the length of the answers array. If it's 10, return null to exit dynamic block
+
+//   // Look at the last answer, if it was correct, make the next question harder by setting a value for left and right
+//   // If it was wrong, make the next question easier by setting a value for left and right
+
+//   return { component: 'HSLColorCodes', parameters: { left: 0, right: 100 }, correctAnswer: [{ id: 'buttonResponse', answer: 'Right' }] };
+// }
