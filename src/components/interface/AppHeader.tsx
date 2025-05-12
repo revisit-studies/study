444d8e89c068b2f51469e861e8b7f958cd89d008
Jay <76601570+yeonkim1213@users.noreply.github.com>
2025-05-12 14:03:43 -0600

    Remove duplicate check for showTitleBar

--- src/components/interface/AppHeader.tsx
+++ src/components/interface/AppHeader.tsx
@@ -88,90 +88,89 @@ export function AppHeader({ studyNavigatorEnabled, dataCollectionEnabled }: { st
 
   return (
     <AppShell.Header p="md">
-      {studyConfig?.uiConfig.showTitleBar === false ? null : (
-        <Grid mt={-7} align="center">
-          <Grid.Col span={4}>
-            <Flex align="center">
-              <Image w={40} src={`${PREFIX}${logoPath}`} alt="Study Logo" />
-              <Space w="md" />
-              {studyConfig?.uiConfig.showTitle !== false ? (
-                <Title
-                  ref={titleRef}
-                  order={4}
-                  style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
-                  title={isTruncated ? studyConfig?.studyMetadata.title : undefined}
-                >
-                  {studyConfig?.studyMetadata.title}
-                </Title>
-              ) : null }
-            </Flex>
-          </Grid.Col>
+      <Grid mt={-7} align="center">
+        <Grid.Col span={4}>
+          <Flex align="center">
+            <Image w={40} src={`${PREFIX}${logoPath}`} alt="Study Logo" />
+            <Space w="md" />
+            {studyConfig?.uiConfig.showTitle !== false ? (
+              <Title
+                ref={titleRef}
+                order={4}
+                style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                title={isTruncated ? studyConfig?.studyMetadata.title : undefined}
+              >
+                {studyConfig?.studyMetadata.title}
+              </Title>
+            ) : null }
+          </Flex>
+        </Grid.Col>
 
-          <Grid.Col span={4}>
-            {withProgressBar && (
+        <Grid.Col span={4}>
+          {withProgressBar && (
             <Progress radius="md" size="lg" value={progressPercent} />
-            )}
-          </Grid.Col>
+          )}
+        </Grid.Col>
 
-          <Grid.Col span={4}>
-            <Group wrap="nowrap" justify="right">
-              {isRecording ? (
-                <Group ml="xl" gap={20} wrap="nowrap">
-                  <Text color="red">Recording audio</Text>
-                  <RecordingAudioWaveform />
-                </Group>
-              ) : null}
-              {!dataCollectionEnabled && <Tooltip multiline withArrow arrowSize={6} w={300} label="This is a demo version of the study, we’re not collecting any data."><Badge size="lg" color="orange">Demo Mode</Badge></Tooltip>}
-              {studyConfig?.uiConfig.helpTextPath !== undefined && (
+        <Grid.Col span={4}>
+          <Group wrap="nowrap" justify="right">
+            {isRecording ? (
+              <Group ml="xl" gap={20} wrap="nowrap">
+                <Text color="red">Recording audio</Text>
+                <RecordingAudioWaveform />
+              </Group>
+            ) : null}
+            {!dataCollectionEnabled && <Tooltip multiline withArrow arrowSize={6} w={300} label="This is a demo version of the study, we’re not collecting any data."><Badge size="lg" color="orange">Demo Mode</Badge></Tooltip>}
+            {studyConfig?.uiConfig.helpTextPath !== undefined && (
               <Button
                 variant="outline"
                 onClick={() => { storeDispatch(toggleShowHelpText()); storeDispatch(incrementHelpCounter({ identifier: `${currentComponent}_${currentStep}` })); }}
               >
                 Help
               </Button>
-              )}
+            )}
 
-              <Menu
-                shadow="md"
-                width={200}
-                withinPortal
-                opened={menuOpened}
-                onChange={setMenuOpened}
-              >
-                <Menu.Target>
-                  <ActionIcon size="lg" className="studyBrowserMenuDropdown" variant="subtle" color="gray">
-                    <IconDotsVertical />
-                  </ActionIcon>
-                </Menu.Target>
-                <Menu.Dropdown>
-                  {studyNavigatorEnabled && (
+            <Menu
+              shadow="md"
+              width={200}
+              withinPortal
+              opened={menuOpened}
+              onChange={setMenuOpened}
+            >
+              <Menu.Target>
+                <ActionIcon size="lg" className="studyBrowserMenuDropdown" variant="subtle" color="gray">
+                  <IconDotsVertical />
+                </ActionIcon>
+              </Menu.Target>
+              <Menu.Dropdown>
+                {studyNavigatorEnabled && (
                   <Menu.Item
                     leftSection={<IconSchema size={14} />}
                     onClick={() => storeDispatch(toggleStudyBrowser())}
                   >
                     Study Browser
                   </Menu.Item>
-                  )}
-                  <Menu.Item
-                    component="a"
-                    href={
+                )}
+                <Menu.Item
+                  component="a"
+                  href={
                         studyConfig !== null
                           ? `mailto:${studyConfig.uiConfig.contactEmail}`
                           : undefined
                       }
-                    leftSection={<IconMail size={14} />}
-                  >
-                    Contact
-                  </Menu.Item>
-                  {studyNavigatorEnabled && (
+                  leftSection={<IconMail size={14} />}
+                >
+                  Contact
+                </Menu.Item>
+                {studyNavigatorEnabled && (
                   <Menu.Item
                     leftSection={<IconUserPlus size={14} />}
                     onClick={() => getNewParticipant(storageEngine, studyConfig, metadata, studyHref)}
                   >
                     Next Participant
                   </Menu.Item>
-                  )}
-                  {studyNavigatorEnabled && (
+                )}
+                {studyNavigatorEnabled && (
                   <Menu.Item
                     leftSection={<IconChartHistogram size={14} />}
                     component="a"
@@ -179,13 +178,12 @@ export function AppHeader({ studyNavigatorEnabled, dataCollectionEnabled }: { st
                   >
                     Analyze & Manage
                   </Menu.Item>
-                  )}
-                </Menu.Dropdown>
-              </Menu>
-            </Group>
-          </Grid.Col>
-        </Grid>
-      )}
+                )}
+              </Menu.Dropdown>
+            </Menu>
+          </Group>
+        </Grid.Col>
+      </Grid>
     </AppShell.Header>
   );
 }
