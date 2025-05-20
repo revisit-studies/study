dc8110a79e6ed9114f94fc87903a4b7e696a3405
Jay <76601570+yeonkim1213@users.noreply.github.com>
2025-05-20 09:32:19 -0600

    Fix bug in handling next element of dynamic block's previous button

--- src/store/hooks/usePreviousStep.ts
+++ src/store/hooks/usePreviousStep.ts
@@ -3,6 +3,8 @@ import { useNavigate, useParams } from 'react-router';
 import { useCurrentStep, useStudyId } from '../../routes/utils';
 import { useIsAnalysis } from './useIsAnalysis';
 import { decryptIndex, encryptIndex } from '../../utils/encryptDecryptIndex';
+import { useStudyConfig } from './useStudyConfig';
+import { getSequenceFlatMap } from '../../utils/getSequenceFlatMap';
 
 export function usePreviousStep() {
   const currentStep = useCurrentStep();
@@ -10,6 +12,7 @@ export function usePreviousStep() {
   const studyId = useStudyId();
   const navigate = useNavigate();
   const isAnalysis = useIsAnalysis();
+  const studyConfig = useStudyConfig();
 
   // Status of the previous button. If false, the previous button should be disabled
   const isPreviousDisabled = typeof currentStep !== 'number' || isAnalysis || currentStep <= 0;
@@ -20,18 +23,31 @@ export function usePreviousStep() {
     }
 
     const previousStep = currentStep - 1;
+    const flatSequence = getSequenceFlatMap(studyConfig.sequence);
+    const currentComponentId = flatSequence[currentStep];
+    const currentComponent = studyConfig.components[currentComponentId];
 
     if (funcIndex) {
       // If we're at the first element of a dynamic block, exit the dynamic block
       if (decryptIndex(funcIndex) === 0) {
         navigate(`/${studyId}/${encryptIndex(previousStep)}${window.location.search}`);
       } else {
+        // Stay in the dynamic block but go to previous element
         navigate(`/${studyId}/${encryptIndex(currentStep)}/${encryptIndex(decryptIndex(funcIndex) - 1)}${window.location.search}`);
+      }
+    } else if (typeof currentComponent === 'object' && 'functionPath' in currentComponent && 'id' in currentComponent && 'order' in currentComponent) {
+      // If current component is a dynamic block, check if next component has previousButton
+      if (currentStep + 1 < flatSequence.length) {
+        const nextComponent = flatSequence[currentStep + 1];
+        const nextConfig = studyConfig.components[nextComponent];
+        if (nextConfig?.previousButton) {
+          navigate(`/${studyId}/${encryptIndex(currentStep)}/${encryptIndex(0)}${window.location.search}`);
+        }
       }
     } else {
       navigate(`/${studyId}/${encryptIndex(previousStep)}${window.location.search}`);
     }
-  }, [currentStep, funcIndex, navigate, studyId]);
+  }, [currentStep, funcIndex, navigate, studyId, studyConfig]);
 
   return {
     isPreviousDisabled,
