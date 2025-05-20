2c9fe7fc58ef5af0bb1c005588264aed816b8da4
Jay <76601570+yeonkim1213@users.noreply.github.com>
2025-05-20 00:36:48 -0600

    Add exiting dynamic block if previous button clicked for the first element

--- src/store/hooks/usePreviousStep.ts
+++ src/store/hooks/usePreviousStep.ts
@@ -3,6 +3,9 @@ import { useNavigate, useParams } from 'react-router';
 import { useCurrentStep, useStudyId } from '../../routes/utils';
 import { useIsAnalysis } from './useIsAnalysis';
 import { decryptIndex, encryptIndex } from '../../utils/encryptDecryptIndex';
+import { useStoreSelector } from '../store';
+import { getSequenceFlatMap } from '../../utils/getSequenceFlatMap';
+import { useStudyConfig } from './useStudyConfig';
 
 export function usePreviousStep() {
   const currentStep = useCurrentStep();
@@ -10,6 +13,9 @@ export function usePreviousStep() {
   const studyId = useStudyId();
   const navigate = useNavigate();
   const isAnalysis = useIsAnalysis();
+  const sequence = useStoreSelector((state) => state.sequence);
+  const flatSequence = getSequenceFlatMap(sequence);
+  const studyConfig = useStudyConfig();
 
   // Status of the previous button. If false, the previous button should be disabled
   const isPreviousDisabled = typeof currentStep !== 'number' || isAnalysis || currentStep <= 0;
@@ -21,12 +27,17 @@ export function usePreviousStep() {
 
     const previousStep = currentStep - 1;
 
-    if (funcIndex && decryptIndex(funcIndex) >= 0) {
-      navigate(`/${studyId}/${encryptIndex(currentStep)}/${encryptIndex(decryptIndex(funcIndex) - 1)}${window.location.search}`);
+    if (funcIndex) {
+      // If we're at the first element of a dynamic block, exit the dynamic block
+      if (decryptIndex(funcIndex) === 0) {
+        navigate(`/${studyId}/${encryptIndex(previousStep)}${window.location.search}`);
+      } else {
+        navigate(`/${studyId}/${encryptIndex(currentStep)}/${encryptIndex(decryptIndex(funcIndex) - 1)}${window.location.search}`);
+      }
     } else {
       navigate(`/${studyId}/${encryptIndex(previousStep)}${window.location.search}`);
     }
-  }, [currentStep, funcIndex, navigate, studyId]);
+  }, [currentStep, funcIndex, navigate, studyId, sequence, flatSequence, studyConfig.components]);
 
   return {
     isPreviousDisabled,
