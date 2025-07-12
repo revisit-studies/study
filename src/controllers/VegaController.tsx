0735279cada3f53b93c6b5b8f631027616dc631a
Jay <76601570+yeonkim1213@users.noreply.github.com>
2025-07-11 22:44:17 -0600

    Fix lint error

--- src/controllers/VegaController.tsx
+++ src/controllers/VegaController.tsx
@@ -154,6 +154,6 @@ export function VegaController({ currentConfig, provState }: { currentConfig: Ve
   return (
     <div id={currentConfig.type} style={vegaStyle}>
       <InternalVega spec={structuredClone(vegaConfig)} signalListeners={signalListeners as never} onNewView={(v) => setView(v)} actions={currentConfig.withActions} />
-     </div>
+    </div>
   );
 }
