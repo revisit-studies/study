e5c52e270a97d75e6751aa82211f1076c359bf98
Jay <76601570+yeonkim1213@users.noreply.github.com>
2025-07-30 17:39:43 -0600

    Refactor dynamic block for berlin-num

--- src/public/libraries/berlin-num/assets/dynamic.tsx
+++ src/public/libraries/berlin-num/assets/dynamic.tsx
@@ -1,37 +1,27 @@
 import { JumpFunctionParameters, JumpFunctionReturnVal } from '../../../../store/types';
 
-function getCompletedBerlinAnswers(answers: Record<string, { endTime: number; answer: Record<string, string> }>): string[] {
-  return Object.keys(answers).filter((key) => {
-    const answer = answers[key];
-    return answer?.endTime > -1
-           && (key.includes('berlin-num')
-            || key.includes('q1-choir')
-            || key.includes('q2a-dice')
-            || key.includes('q2b-loaded')
-            || key.includes('q3-poisonous'))
-           && answer?.answer;
-  });
-}
+export default function dynamic({ answers, currentStep, currentBlock }: JumpFunctionParameters<never>): JumpFunctionReturnVal {
+  const filteredAnswers = Object.entries(answers)
+    .filter(([key, value]) => key.startsWith(`${currentBlock}_${currentStep}`) && value.endTime > -1);
 
-export default function dynamic({ answers }: JumpFunctionParameters<never>): JumpFunctionReturnVal {
-  const completedAnswers = getCompletedBerlinAnswers(answers as Record<string, { endTime: number; answer: Record<string, string> }>);
-
-  if (completedAnswers.length === 0) {
+  if (filteredAnswers.length === 0) {
     return { component: '$berlin-num.components.q1-choir-probability' };
   }
 
-  if (completedAnswers.length === 1) {
-    const q1Answer = answers[completedAnswers[0]]?.answer?.['q1-choir-probability'];
+  if (filteredAnswers.length === 1) {
+    const q1Answer = filteredAnswers[0][1]?.answer?.['q1-choir-probability'];
     return {
+      // If q1 is correct, go to q2b. If q1 is incorrect, go to q2a.
       component: q1Answer === '25' ? '$berlin-num.components.q2b-loaded-dice' : '$berlin-num.components.q2a-dice-odd-numbers',
     };
   }
 
-  if (completedAnswers.length === 2) {
-    const q1Answer = answers[completedAnswers[0]]?.answer?.['q1-choir-probability'];
+  if (filteredAnswers.length === 2) {
+    const q1Answer = filteredAnswers[0][1]?.answer?.['q1-choir-probability'];
 
     if (q1Answer === '25') {
-      const q2bAnswer = answers[completedAnswers[1]]?.answer?.['q2b-loaded-dice'];
+      const q2bAnswer = filteredAnswers[1][1]?.answer?.['q2b-loaded-dice'];
+      // If q2b is correct, finish the dynamic block. If q2b is incorrect, go to q3.
       return { component: q2bAnswer === '20' ? null : '$berlin-num.components.q3-poisonous-mushrooms' };
     }
   }
