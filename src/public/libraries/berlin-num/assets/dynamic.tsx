4a0b637b3577b5477b8c636624539b34fec2952c
Jay <76601570+yeonkim1213@users.noreply.github.com>
2025-07-31 18:36:46 -0600

    Fix comparing numeric values in berlin num dynamic blocks

--- src/public/libraries/berlin-num/assets/dynamic.tsx
+++ src/public/libraries/berlin-num/assets/dynamic.tsx
@@ -12,17 +12,17 @@ export default function dynamic({ answers, currentStep, currentBlock }: JumpFunc
     const q1Answer = filteredAnswers[0][1]?.answer?.['q1-choir-probability'];
     return {
       // If q1 is correct, go to q2b. If q1 is incorrect, go to q2a.
-      component: q1Answer === '25' ? '$berlin-num.components.q2b-loaded-dice' : '$berlin-num.components.q2a-dice-odd-numbers',
+      component: q1Answer === 25 ? '$berlin-num.components.q2b-loaded-dice' : '$berlin-num.components.q2a-dice-odd-numbers',
     };
   }
 
   if (filteredAnswers.length === 2) {
     const q1Answer = filteredAnswers[0][1]?.answer?.['q1-choir-probability'];
 
-    if (q1Answer === '25') {
+    if (q1Answer === 25) {
       const q2bAnswer = filteredAnswers[1][1]?.answer?.['q2b-loaded-dice'];
       // If q2b is correct, finish the dynamic block. If q2b is incorrect, go to q3.
-      return { component: q2bAnswer === '20' ? null : '$berlin-num.components.q3-poisonous-mushrooms' };
+      return { component: q2bAnswer === 20 ? null : '$berlin-num.components.q3-poisonous-mushrooms' };
     }
   }
 
