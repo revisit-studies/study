69fb1ecf4e058b8960454d906862ed5c1214fed1
Jay <76601570+yeonkim1213@users.noreply.github.com>
2025-08-13 17:47:42 -0600

    Add timestamp string to replay url

--- src/utils/useNavigateToTrial.tsx
+++ src/utils/useNavigateToTrial.tsx
@@ -3,10 +3,15 @@ import { encryptIndex } from './encryptDecryptIndex';
 import { PREFIX } from './Prefix';
 
 export function useNavigateToTrial() {
-  return useCallback((trialOrder: string, participantId: string, studyId: string) => {
+  return useCallback((trialOrder: string, participantId: string, studyId: string, timeString?: string) => {
     const rejoined = trialOrder.split('_').map((index) => encryptIndex(+index)).join('/');
 
-    const url = `${studyId}/${rejoined}?participantId=${participantId}`;
+    let url = `${studyId}/${rejoined}?participantId=${participantId}`;
+
+    if (timeString) {
+      url += `&t=${timeString}`;
+    }
+
     window.open(`${PREFIX}${url}`, '_blank');
   }, []);
 }
