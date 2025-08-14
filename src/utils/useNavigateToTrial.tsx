bb8ccbddb41b978a2746e75fc2e367ff71b2d9c8
Jay <76601570+yeonkim1213@users.noreply.github.com>
2025-08-13 18:01:30 -0600

    Revert use navigate to trial

--- src/utils/useNavigateToTrial.tsx
+++ src/utils/useNavigateToTrial.tsx
@@ -3,15 +3,9 @@ import { encryptIndex } from './encryptDecryptIndex';
 import { PREFIX } from './Prefix';
 
 export function useNavigateToTrial() {
-  return useCallback((trialOrder: string, participantId: string, studyId: string, timeString?: string) => {
+  return useCallback((trialOrder: string, participantId: string, studyId: string) => {
     const rejoined = trialOrder.split('_').map((index) => encryptIndex(+index)).join('/');
-
-    let url = `${studyId}/${rejoined}?participantId=${participantId}`;
-
-    if (timeString) {
-      url += `&t=${timeString}`;
-    }
-
+    const url = `${studyId}/${rejoined}?participantId=${participantId}`;
     window.open(`${PREFIX}${url}`, '_blank');
   }, []);
 }
