b45d59b6528996b170bdcd9342ffeaaa24e3585e
Hilson Shrestha <hilsonshrestha@gmail.com>
2025-08-16 23:30:24 -0400

    Remove controls from screen recording replay video

--- src/components/screenRecording/ScreenRecordingReplay.tsx
+++ src/components/screenRecording/ScreenRecordingReplay.tsx
@@ -1,17 +1,10 @@
 import {
-  MouseEvent,
-  useCallback,
   useEffect, useMemo, useRef, useState,
 } from 'react';
 import { useSearchParams } from 'react-router';
 import {
-  ActionIcon, Box, Flex, LoadingOverlay,
-  Tooltip,
+  Box, LoadingOverlay,
 } from '@mantine/core';
-import {
-  IconArrowsMaximize, IconPlayerPauseFilled, IconPlayerPlayFilled,
-} from '@tabler/icons-react';
-import { useFullscreen } from '@mantine/hooks';
 import { useStorageEngine } from '../../storage/storageEngineHooks';
 import {
   useFlatSequence,
@@ -21,12 +14,8 @@ import {
 } from '../../store/store';
 import { useCurrentIdentifier } from '../../routes/utils';
 import { useIsAnalysis } from '../../store/hooks/useIsAnalysis';
-import classes from './screenRecording.module.css';
-import { humanReadableDuration } from '../../utils/humanReadableDuration';
 
 export function ScreenRecordingReplay() {
-  const { ref: fullScreenRef, toggle: toggleFullScreen, fullscreen } = useFullscreen();
-
   const [searchParams] = useSearchParams();
   const participantId = useMemo(
     () => searchParams.get('participantId') || undefined,
@@ -34,8 +23,6 @@ export function ScreenRecordingReplay() {
   );
 
   const [isReady, setIsReady] = useState(false);
-
-  const stopTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const [isPlaying, setIsPlaying] = useState(false);
 
@@ -45,16 +32,9 @@ export function ScreenRecordingReplay() {
   const [videoStartTime, setVideoStartTime] = useState(0);
   const [videoEndTime, setVideoEndTime] = useState(0);
 
-  const [hoveredTime, setHoveredTime] = useState(0);
-
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  const timelineRef = useRef<HTMLDivElement | null>(null);
-  const timelineHoverRef = useRef<HTMLDivElement | null>(null);
-  const timelineWrapperRef = useRef<HTMLDivElement | null>(null);
 
   const [duration, setDuration] = useState<number>(0);
-  const [stimulusCurrentTime, setStimulusCurrentTime] = useState<number>(1);
-  const [stimulusDuration, setStimulusDuration] = useState<number>(1);
 
   const { storageEngine } = useStorageEngine();
 
@@ -104,8 +84,6 @@ export function ScreenRecordingReplay() {
 
         setVideoStartTime(stimulusVideoStartTime);
         setVideoEndTime(stimulusVideoEndTime);
-
-        setStimulusDuration(stimulusVideoEndTime - stimulusVideoStartTime);
       }
     }
 
@@ -118,27 +96,13 @@ export function ScreenRecordingReplay() {
   useEffect(() => {
     const video = videoRef.current;
     if (video && videoStartTime > 0 && videoEndTime > 0) {
-      const currentStimulusVideoDuration = (videoEndTime - videoStartTime) / 1000;
       video.currentTime = videoStartTime / 1000;
       video.onpause = () => {
         setIsPlaying(false);
-
-        if (stopTimeoutRef.current) {
-          clearTimeout(stopTimeoutRef.current);
-        }
-        if (timelineRef.current) {
-          const w = ((video.currentTime - videoStartTime / 1000) / currentStimulusVideoDuration) * 100;
-          timelineRef.current.style.width = `${w > 100 ? 100 : w}%`;
-        }
       };
 
       video.ontimeupdate = () => {
-        if (timelineRef.current) {
-          const w = ((video.currentTime - videoStartTime / 1000) / currentStimulusVideoDuration) * 100;
-          timelineRef.current.style.width = `${w > 100 ? 100 : w}%`;
-        }
         if (videoRef.current) {
-          setStimulusCurrentTime(videoRef.current.currentTime * 1000 - videoStartTime);
           if (videoRef.current.currentTime > videoEndTime / 1000) {
             videoRef.current.pause();
           }
@@ -218,82 +182,25 @@ export function ScreenRecordingReplay() {
     ],
   );
 
-  const handleTimelineMouseUp = useCallback((e: MouseEvent<HTMLDivElement>) => {
-    if (videoRef.current) {
-      const parentBounding = timelineWrapperRef.current?.getBoundingClientRect();
-      const percentage = (e.clientX - (parentBounding?.left || 0)) / (parentBounding?.width || 1);
-
-      // get time from percentage
-      videoRef.current.currentTime = (videoStartTime + percentage * (videoEndTime - videoStartTime)) / 1000;
-      setStimulusCurrentTime(videoRef.current.currentTime * 1000 - videoStartTime);
-    }
-  }, [videoStartTime, videoEndTime]);
-
-  const handleTimelineMouseMove = useCallback((e: MouseEvent<HTMLDivElement>) => {
-    if (videoRef.current) {
-      const parentBounding = timelineWrapperRef.current?.getBoundingClientRect();
-      const percentage = (e.clientX - (parentBounding?.left || 0)) / (parentBounding?.width || 1);
-
-      // get time from percentage
-      const currentTime = (videoStartTime + percentage * (videoEndTime - videoStartTime)) / 1000;
-      const targetTime = (currentTime * 1000 - videoStartTime);
-
-      setHoveredTime(targetTime);
-
-      timelineHoverRef.current!.style.width = `${percentage * 100}%`;
-    }
-  }, [videoStartTime, videoEndTime]);
-
   return (
-    <Box pos="relative" ref={fullScreenRef} bg="white" px={fullscreen ? 'lg' : 0}>
-      <Box pos="relative">
-        <video
-          ref={videoRef}
-          width="100%"
-          style={{
-            background: 'black',
-            maxWidth: 'calc(100vw)',
-            maxHeight: 'calc(100vh - 200px)',
-            display: 'block',
-            margin: '20px auto',
-            height: isReady ? 'auto' : 200,
-            border: `5px solid ${isPlaying ? '#ccc' : 'black'}`,
-          }}
-        >
-          <source type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <LoadingOverlay visible={!isReady} zIndex={1000} overlayProps={{ blur: 5, bg: 'rgba(255, 255, 255, .98)' }} />
-      </Box>
-      <Flex ref={timelineWrapperRef} className={classes.timeline} onMouseUp={handleTimelineMouseUp} onMouseMove={handleTimelineMouseMove}>
-        <div className={classes.timelineBar}>
-          <div ref={timelineRef} className={classes.timelineInner} />
-          <Tooltip label={humanReadableDuration(hoveredTime)} position="top-end" w={80} offset={{ mainAxis: 15, crossAxis: 40 }} ta="center" withArrow arrowOffset={37} arrowSize={6}>
-            <div ref={timelineHoverRef} className={classes.timelineInnerHover} />
-          </Tooltip>
-        </div>
-      </Flex>
-
-      <Flex h={20} align="center">
-        {isPlaying ? (
-          <ActionIcon variant="filled" aria-label="Pause" onClick={() => videoRef.current?.pause()} color="green">
-            <IconPlayerPauseFilled />
-          </ActionIcon>
-        ) : (
-          <ActionIcon variant="filled" aria-label="Play" onClick={() => videoRef.current?.play()}>
-            <IconPlayerPlayFilled />
-          </ActionIcon>
-        )}
-        <Flex flex={1} justify="center">
-          {humanReadableDuration(stimulusCurrentTime)}
-          {' '}
-          /
-          {humanReadableDuration(stimulusDuration)}
-        </Flex>
-        <ActionIcon variant="filled" aria-label="Play" onClick={toggleFullScreen}>
-          <IconArrowsMaximize />
-        </ActionIcon>
-      </Flex>
+    <Box pos="relative">
+      <video
+        ref={videoRef}
+        width="100%"
+        style={{
+          background: 'black',
+          maxWidth: '100%',
+          maxHeight: 'calc(100vh - 200px)',
+          display: 'block',
+          margin: '20px auto',
+          height: isReady ? 'auto' : 200,
+          border: `5px solid ${isPlaying ? '#ccc' : 'black'}`,
+        }}
+      >
+        <source type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <LoadingOverlay visible={!isReady} zIndex={1000} overlayProps={{ blur: 5, bg: 'rgba(255, 255, 255, .98)' }} />
     </Box>
   );
 }
