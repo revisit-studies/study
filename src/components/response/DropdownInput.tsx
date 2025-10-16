c5c6ece55f2a14c87f6328d0ea8ee9914a6a913d
Jay <76601570+yeonkim1213@users.noreply.github.com>
2025-10-16 00:38:45 -0600

    Fix placeholder

--- src/components/response/DropdownInput.tsx
+++ src/components/response/DropdownInput.tsx
@@ -35,7 +35,7 @@ export function DropdownInput({
         disabled={disabled}
         label={prompt.length > 0 && <InputLabel prompt={prompt} required={required} index={index} enumerateQuestions={enumerateQuestions} infoText={infoText} />}
         description={secondaryText}
-        placeholder={Array.isArray(answer.value) && answer.value.length === 0 ? placeholder : undefined}
+        placeholder={Array.isArray(answer.value) && answer.value.length === 0 ? placeholder : null}
         data={optionsAsStringOptions}
         radius="md"
         size="md"
