5aaa3eb41b486c653504a46bacd891cc983d5f4a
Jack Wilburn <jackwilburn@tutanota.com>
2025-10-17 16:04:32 -0600

    Fix redirect to url

--- src/storage/engines/SupabaseStorageEngine.ts
+++ src/storage/engines/SupabaseStorageEngine.ts
@@ -697,7 +697,7 @@ export class SupabaseStorageEngine extends CloudStorageEngine {
   async login() {
     const { error } = await this.supabase.auth.signInWithOAuth({
       provider: 'github',
-      options: { redirectTo: window.location.origin },
+      options: { redirectTo: window.location.href.replace('/settings', '') },
     });
 
     if (error) throw error;
