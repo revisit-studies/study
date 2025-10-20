0c0234c20ab264eb2f2ed01ffae7bd4e7443169a
Jay <76601570+yeonkim1213@users.noreply.github.com>
2025-10-20 13:51:20 -0600

    Update revisit modes style

--- src/analysis/individualStudy/management/RevisitModesItem.tsx
+++ src/analysis/individualStudy/management/RevisitModesItem.tsx
@@ -1,9 +1,6 @@
 import {
-  Switch, Text, Title, Tooltip, Flex,
+  Switch, Text, Title, Flex,
 } from '@mantine/core';
-import {
-  IconSchema, IconSchemaOff, IconGraph, IconGraphOff, IconChartBarPopular, IconChartBarOff,
-} from '@tabler/icons-react';
 import { useEffect, useState } from 'react';
 import { useStorageEngine } from '../../../storage/storageEngineHooks';
 
@@ -50,54 +47,42 @@ export function RevisitModesItem({ studyId }: { studyId: string }) {
           ReVISit modes let you control what happens when you visit your study when designing/debugging your study, actively collecting data, and analyzing data when data collection is concluded.
         </Text>
 
-        <Flex justify="space-between">
+        <Flex gap="xs">
           <Title order={5}>Data Collection</Title>
-          <Tooltip label={dataCollectionEnabled ? 'Data Collection Enabled' : 'Data Collection Disabled'} refProp="rootRef">
-            <Switch
-              size="md"
-              color="green"
-              onLabel={<IconChartBarPopular size={18} />}
-              offLabel={<IconChartBarOff size={18} color="red" />}
-              checked={dataCollectionEnabled}
-              onChange={(event) => handleSwitch('dataCollectionEnabled', event.currentTarget.checked)}
-            />
-          </Tooltip>
+          <Switch
+            size="sm"
+            checked={dataCollectionEnabled}
+            onChange={(event) => handleSwitch('dataCollectionEnabled', event.currentTarget.checked)}
+            mt="3px"
+          />
         </Flex>
-        <Text mb="sm" w="90%">
+        <Text mb="sm">
           When enabling data collection, data is written to your data store. Make sure to turn this on when actively collecting data, and turn it off after you have concluded data collection, so you don&apos;t pollute your data by accident.
         </Text>
 
-        <Flex justify="space-between">
+        <Flex gap="xs">
           <Title order={5}>Development Mode</Title>
-          <Tooltip label={developmentModeEnabled ? 'Development Mode Enabled' : 'Development Mode Disabled'} refProp="rootRef">
-            <Switch
-              size="md"
-              color="green"
-              onLabel={<IconSchema size={18} />}
-              offLabel={<IconSchemaOff size={18} color="red" />}
-              checked={developmentModeEnabled}
-              onChange={(event) => handleSwitch('developmentModeEnabled', event.currentTarget.checked)}
-            />
-          </Tooltip>
+          <Switch
+            size="sm"
+            checked={developmentModeEnabled}
+            onChange={(event) => handleSwitch('developmentModeEnabled', event.currentTarget.checked)}
+            mt="3px"
+          />
         </Flex>
-        <Text mb="sm" w="90%">
+        <Text mb="sm">
           Debug and development mode enables the study navigator, letting visitors jump between tasks. It also disables device checks, such as minimum screen size, and lets you navigate to the analytics interface.
         </Text>
 
-        <Flex justify="space-between">
+        <Flex gap="xs">
           <Title order={5}>Data Sharing</Title>
-          <Tooltip label={dataSharingEnabled ? 'Share Data and Make Analytics Interface Public' : 'Share Data and Make Analytics Interface Private'} refProp="rootRef">
-            <Switch
-              size="md"
-              color="green"
-              onLabel={<IconGraph size={18} />}
-              offLabel={<IconGraphOff size={18} color="red" />}
-              checked={dataSharingEnabled}
-              onChange={(event) => handleSwitch('dataSharingEnabled', event.currentTarget.checked)}
-            />
-          </Tooltip>
+          <Switch
+            size="sm"
+            checked={dataSharingEnabled}
+            onChange={(event) => handleSwitch('dataSharingEnabled', event.currentTarget.checked)}
+            mt="3px"
+          />
         </Flex>
-        <Text w="90%">
+        <Text>
           When you enabling data sharing, anyone visiting your study website can access the analytics interface and download all of your data. If you disable data sharing, only authenticated users with permissions can access the analytics interface and the associated data.
         </Text>
       </>
