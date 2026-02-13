db3963beb42b0f22a9cc92695838f6730132153c
Jay Kim <76601570+yeonkim1213@users.noreply.github.com>
2026-02-13 00:47:59 -0700

    Add normalizing ordered options to string option

--- src/components/response/ButtonsInput.tsx
+++ src/components/response/ButtonsInput.tsx
@@ -33,7 +33,7 @@ export function ButtonsInput({
   const storedAnswer = useStoredAnswer();
   const optionOrders: Record<string, StringOption[]> = useMemo(() => (storedAnswer ? storedAnswer.optionOrders : {}), [storedAnswer]);
 
-  const orderedOptions = useMemo(() => optionOrders[response.id] || options, [optionOrders, options, response.id]);
+  const orderedOptions = useMemo(() => optionOrders[response.id] || options.map((option) => (typeof option === 'string' ? { label: option, value: option } : option)), [optionOrders, options, response.id]);
 
   const error = useMemo(() => generateErrorMessage(response, answer, orderedOptions), [response, answer, orderedOptions]);
 
