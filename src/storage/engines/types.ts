a43945d8c198ae0bdeca26b546c54e76956decf3
Jack Wilburn <jackwilburn@tutanota.com>
2026-02-21 16:01:39 -0700

    Load into the old version of the config if resuming from old config

--- src/storage/engines/types.ts
+++ src/storage/engines/types.ts
@@ -406,14 +406,15 @@ export abstract class StorageEngine {
     );
     await this._cacheStorageObject(`configs/${configHash}`, 'config');
 
-    // Clear sequence array and current participant data if the config has changed
+    // Clear sequence array if the config has changed.
+    // Keep currentParticipantId so existing participant sessions can continue
+    // against their original participantConfigHash.
     if (currentConfigHash && currentConfigHash !== configHash) {
       try {
         await this._deleteFromStorage('', 'sequenceArray');
       } catch {
         // pass, if this happens, we didn't have a sequence array yet
       }
-      await this.clearCurrentParticipantId();
     }
 
     await this._setCurrentConfigHash(configHash);
