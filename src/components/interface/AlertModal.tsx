2379b1fd359f613612a497a64bf24df96033f048
Jay Kim <76601570+yeonkim1213@users.noreply.github.com>
2026-02-21 15:48:56 -0700

    Add email template

--- src/components/interface/AlertModal.tsx
+++ src/components/interface/AlertModal.tsx
@@ -2,7 +2,9 @@ import {
   ActionIcon, Alert, Anchor, Box, Button, Code, Group, Modal, Text,
 } from '@mantine/core';
 import { IconAlertCircle, IconCheck, IconCopy } from '@tabler/icons-react';
-import { useCallback, useEffect, useState } from 'react';
+import {
+  useCallback, useEffect, useMemo, useState,
+} from 'react';
 import { useStoreActions, useStoreDispatch, useStoreSelector } from '../../store/store';
 
 export function AlertModal() {
@@ -23,7 +25,18 @@ export function AlertModal() {
   useEffect(() => setOpened(alertModal.show), [alertModal.show]);
   const isStorageEngineAlert = alertModal.title === 'Failed to connect to the storage engine';
 
-  const diagnosticsMessage = `Study ID: ${studyId}\nURL: ${window.location.href}\nParticipant ID: ${participantId}\nTimestamp (UTC): ${new Date().toISOString()}\nStorage Engine: ${import.meta.env.VITE_STORAGE_ENGINE}\nUser Agent: ${participantMetadata.userAgent}\nResolution: ${JSON.stringify(participantMetadata.resolution, null, 2)}\nIP: ${participantMetadata.ip}\nLanguage: ${participantMetadata.language}`;
+  const diagnosticsMessage = useMemo(() => {
+    if (!opened || !isStorageEngineAlert) {
+      return '';
+    }
+    return `Study ID: ${studyId}\nURL: ${window.location.href}\nParticipant ID: ${participantId}\nTimestamp (UTC): ${new Date().toISOString()}\nStorage Engine: ${import.meta.env.VITE_STORAGE_ENGINE}\nUser Agent: ${participantMetadata.userAgent}\nResolution: ${JSON.stringify(participantMetadata.resolution, null, 2)}\nIP: ${participantMetadata.ip}\nLanguage: ${participantMetadata.language}`;
+  }, [opened, isStorageEngineAlert, studyId, participantId, participantMetadata]);
+
+  const mailTemplate = useMemo(() => {
+    const subject = encodeURIComponent(`Storage Connection Issue (${studyId})`);
+    const body = encodeURIComponent(`I encountered a storage connection issue while taking a study.\n\n Warning message:\n${alertModal.message}\n\nDiagnostics information:\n${diagnosticsMessage}`);
+    return `mailto:${studyConfig.uiConfig.contactEmail}?subject=${subject}&body=${body}`;
+  }, [diagnosticsMessage, studyConfig.uiConfig.contactEmail, studyId]);
 
   const handleCopyMessage = useCallback(async () => {
     await navigator.clipboard.writeText(diagnosticsMessage);
@@ -47,16 +60,19 @@ export function AlertModal() {
         onClose={close}
         styles={{ root: { backgroundColor: 'unset' } }}
       >
-        <Text my="md">
+        <Text my="xs">
           {alertModal.message}
           {isStorageEngineAlert && (
             <>
-              {' '}
+              <br />
+              <br />
               Please email if you need help
-              <Anchor href={`mailto:${studyConfig.uiConfig.contactEmail}`} ml={4}>
+              {' '}
+              <Anchor href={mailTemplate}>
                 {studyConfig.uiConfig.contactEmail}
               </Anchor>
-              {' and include the following details to help us troubleshoot the issue:'}
+              {' '}
+              and include the following details to help us troubleshoot the issue:
             </>
           )}
         </Text>
