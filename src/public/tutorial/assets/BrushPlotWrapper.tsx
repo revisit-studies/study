c7fbb950944977b90a39c91bcbdc4937e8a2a0f5
Andrew McNutt <mcnutt.andrew@gmail.com>
2026-04-29 09:50:51 -0600

    Updated BrushPlotWrapper.tsx

--- src/public/tutorial/assets/BrushPlotWrapper.tsx
+++ src/public/tutorial/assets/BrushPlotWrapper.tsx
@@ -1,13 +1,12 @@
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import React from 'react';
-// import { BrushParams, BrushState } from '../../example-brush-interactions/assets/types';
-// import { BrushPlot } from '../../example-brush-interactions/assets/BrushPlot';
-// import { ParticipantData } from '../../../storage/types';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react';
+import { BrushParams, BrushState } from '../../example-brush-interactions/assets/types';
+import { BrushPlot } from '../../example-brush-interactions/assets/BrushPlot';
+import { ParticipantData } from '../../../storage/types';
 
-// export default function BrushPlotWrapper({
-//   state, params, answers, onStateChange,
-// } : {state?: BrushState, params: BrushParams, answers: ParticipantData['answers'], onStateChange?: (b: BrushState) => void}) {
-//   return <BrushPlot parameters={params} setAnswer={() => null} provenanceState={state ? { all: state } as any : undefined} updateState={onStateChange || (() => null)} answers={answers} />;
-// }
+export default function BrushPlotWrapper({
+  state, params, answers, onStateChange,
+} : {state?: BrushState, params: BrushParams, answers: ParticipantData['answers'], onStateChange?: (b: BrushState) => void}) {
+  return <BrushPlot parameters={params} setAnswer={() => null} provenanceState={state ? { all: state } as any : undefined} updateState={onStateChange || (() => null)} answers={answers} />;
+}
 
-export default function BrushPlotWrapper() {}
