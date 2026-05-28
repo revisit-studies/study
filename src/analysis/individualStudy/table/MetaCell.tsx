d9579631915e3d2ddc7f65892ca9b07d6e10d133
Jay Kim <76601570+yeonkim1213@users.noreply.github.com>
2026-05-28 12:41:35 -0600

    Match font size in participant view

--- src/analysis/individualStudy/table/MetaCell.tsx
+++ src/analysis/individualStudy/table/MetaCell.tsx
@@ -1,29 +1,29 @@
-import { Spoiler, Stack, Box } from '@mantine/core';
+import { Box, Spoiler, Stack } from '@mantine/core';
 import { ParticipantMetadata } from '../../../store/types';
 
 export function MetaCell({ metaData }: { metaData: ParticipantMetadata | undefined }) {
   return (
     <Spoiler w={200} hideLabel="hide" maxHeight={50} showLabel="more">
       <Stack gap="xs">
-        <Box>
+        <Box fz="sm">
           <strong>Resolution:</strong>
           {' '}
-          {JSON.stringify(metaData?.resolution, null, 2)}
+          {metaData?.resolution ? JSON.stringify(metaData.resolution, null, 2) : 'N/A'}
         </Box>
-        <Box>
+        <Box fz="sm">
           <strong>User Agent:</strong>
           {' '}
-          {metaData?.userAgent}
+          {metaData?.userAgent || 'N/A'}
         </Box>
-        <Box>
+        <Box fz="sm">
           <strong>IP:</strong>
           {' '}
-          {metaData?.ip}
+          {metaData?.ip || 'N/A'}
         </Box>
-        <Box>
+        <Box fz="sm">
           <strong>Language:</strong>
           {' '}
-          {metaData?.language}
+          {metaData?.language || 'N/A'}
         </Box>
       </Stack>
     </Spoiler>
