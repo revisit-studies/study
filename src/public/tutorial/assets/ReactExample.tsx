f226f02fa5e55db2279ef4addf91033becbe2d67
Jack Wilburn <jackwilburn@tutanota.com>
2026-07-21 17:31:13 -0600

    Avoid duplicate tutorial provenance graphs

--- src/public/tutorial/assets/ReactExample.tsx
+++ src/public/tutorial/assets/ReactExample.tsx
@@ -1,50 +1,25 @@
-import { useCallback, useMemo, useState } from 'react';
-import { Registry } from '@trrack/core';
+import { useCallback, useState } from 'react';
 import BrushPlotWrapper from './BrushPlotWrapper';
 import { StimulusParams } from '../../../store/types';
 import { BrushParams, BrushState } from '../../example-brush-interactions/assets/types';
 
+type BrushProvenanceState = { all: { brush: BrushState } };
+
 export default function ReactExample({
   parameters, setAnswer, provenanceState, useTrrack,
-}: StimulusParams<BrushParams, BrushState>) {
+}: StimulusParams<BrushParams, BrushProvenanceState>) {
   const [brushState, setBrushState] = useState<BrushState>();
-
-  // creating provenance tracking
-  const { actions, registry } = useMemo(() => {
-    const reg = Registry.create();
-
-    const brush = reg.register('brush', (state, currBrush: BrushState) => {
-      state.brush = currBrush;
-      return state;
-    });
-
-    return {
-      actions: {
-        brush,
-      },
-      registry: reg,
-    };
-  }, []);
-  const trrack = useTrrack({
-    registry,
-    initialState: {
-      brush: {
-        hasBrush: false, x1: null, x2: null, y1: null, y2: null, ids: [],
-      },
-    },
-  });
 
   const onStateChange = useCallback((b: BrushState) => {
     setBrushState(b);
-    trrack.apply('Brush', actions.brush(b));
 
     setAnswer({
       answers: {},
       status: true,
     });
-  }, [actions, setAnswer, trrack]);
+  }, [setAnswer]);
 
   return (
-    <BrushPlotWrapper params={parameters} state={provenanceState || brushState} onStateChange={onStateChange} answers={{}} useTrrack={useTrrack} />
+    <BrushPlotWrapper params={parameters} state={provenanceState?.all.brush ?? brushState} onStateChange={onStateChange} answers={{}} useTrrack={useTrrack} />
   );
 }
